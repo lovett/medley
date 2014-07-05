0e8eebe1d1c96ae0919e0906bc422a6e357619f6
Bill Lovett <bill@ilovett.com>
2014-07-04 21:15:51 -0400

    /geoip and /phone endpoints
    The geoip endpoint is based on Maxmind's GeoLite2 City database.
    
    The phone endpoint is based on dbPedia and Google Gecoding APIs, but needs further work

--- medley.py
+++ medley.py
@@ -3,6 +3,12 @@ import os.path
 import os
 import pwd
 import subprocess
+import geoip2.database
+import csv
+import re
+import urllib
+import urllib.parse
+import json
 
 class MedleyServer(object):
     @cherrypy.expose
@@ -43,6 +49,65 @@ class MedleyServer(object):
     @cherrypy.tools.json_out()
     def headers(self):
         return cherrypy.request.headers
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def geoip(self, address):
+        dbPath = cherrypy.request.app.config["geo"].get("ip.city")
+
+        reader = geoip2.database.Reader(dbPath)
+        response = reader.city(address)
+        reader.close()
+
+        return {
+            "country": response.country.name,
+            "city": response.city.name,
+            "timezone": response.location.time_zone,
+            "latlong": [response.location.latitude, response.location.longitude]
+        }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def phone(self, number):
+        """
+        Given a US phone number, return the corresponding state its area code belongs to.
+        """
+        number = re.sub("\D", "", number)
+        number = number.lstrip("1")
+
+        areaCode = number[:3]
+
+        params = {
+            "default-graph-uri": "http://dbpedia.org",
+            "query": "select * where { dbpedia:Area_code_%s geo:lat ?lat; geo:long ?long }" % (areaCode),
+            "format": "json",
+            "timeout": "1000"
+        }
+
+        sparqlQuery = "http://dbpedia.org/sparql?%s" % urllib.parse.urlencode(params)
+
+        with urllib.request.urlopen(sparqlQuery) as request:
+            sparqlResult = json.loads(request.read().decode("utf-8"))
+
+        firstResult = sparqlResult["results"]["bindings"][0]
+
+        params = {
+            "latlng": "%s,%s" % (firstResult["lat"].get("value"),
+                                 firstResult["long"].get("value")),
+            "result_type": "locality|administrative_area_level_1",
+            "key": cherrypy.request.app.config["google"].get("api.key")
+        }
+
+        geoQuery = "https://maps.googleapis.com/maps/api/geocode/json?%s" % urllib.parse.urlencode(params)
+
+        with urllib.request.urlopen(geoQuery) as request:
+            geoResult = json.loads(request.read().decode("utf-8"))
+
+        return {
+            "location": geoResult["results"][0]["formatted_address"],
+            "reverseLookup": "http://www.whitepages.com/phone/%s" % (number),
+            "wikipedia": "http://en.wikipedia.org/wiki/Area_code_%s" % (areaCode)
+        }
 
 if __name__ == "__main__":
     appRoot = os.path.dirname(os.path.abspath(__file__))
