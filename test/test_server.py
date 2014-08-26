a0e07d223c1d1822c36625d8a6b2a267a4508191
Bill Lovett <bill@ilovett.com>
2014-08-25 21:30:53 -0400

    Rework existing tests

--- test/test_server.py
+++ test/test_server.py
@@ -29,48 +29,100 @@ def setup_module():
 def teardown_module():
     cherrypy.engine.exit()
 
+def getResponseBody(response):
+    return response.collapse_body().decode("utf-8")
+
+def getResponseBodyJson(response):
+    body = getResponseBody(response)
+    return json.loads(body)
+
 class TestMedleyServer(BaseCherryPyTestCase):
-    def test_index(self):
-        """ The index should return successfully, in spite of being unused """
-        response = self.request('/')
-        self.assertEqual(response.status, '200 OK')
+    def test_indexReturnsHtml(self):
+        """ The index should return html by default """
+        headers = {
+            "Accept": "*/*"
+        }
+        response = self.request("/", headers=headers)
+        body = getResponseBody(response)
+        self.assertEqual(response.status, "200 OK")
+        self.assertEqual("<html>" in body, True)
+
+    def test_indexReturnsJson(self):
+        """ The index should return json if requested """
+        headers = {
+            "Accept": "application/json"
+        }
+        response = self.request("/", headers=headers)
+        body = getResponseBodyJson(response)
+        self.assertEqual(body["message"], "hello")
 
     def test_ipNoTokenRequiresAuth(self):
         """ Calling /ip without a token requires authentication """
-        response = self.request('/ip', headers={"REMOTE-ADDR": "1.1.1.1"})
+        headers = {
+            "Remote-Addr": "1.1.1.1"
+        }
+        response = self.request('/ip', headers=headers)
         self.assertEqual(response.status, '401 Unauthorized')
 
     def test_ipNoToken(self):
         """ Calling /ip without a token should emit the caller's IP """
-        response = self.request('/ip', headers={"REMOTE-ADDR": "1.1.1.1", "Authorization": "Basic dGVzdDp0ZXN0"})
-        result = response.collapse_body().decode('utf=8')
+        headers = {
+            "Remote-Addr": "1.1.1.1",
+            "Authorization": "Basic dGVzdDp0ZXN0"
+        }
+        response = self.request('/ip', headers=headers)
+        body = getResponseBody(response)
         self.assertEqual(response.status, '200 OK')
-        self.assertEqual(result, '1.1.1.1')
+        self.assertEqual(body, '1.1.1.1')
 
     def test_ipNoTokenJson(self):
         """ The /ip endpoint doesn't accept json """
-        response = self.request('/ip', headers={"REMOTE-ADDR": "1.1.1.1", "Authorization": "Basic dGVzdDp0ZXN0", "Accept": "application/json"})
+        headers = {
+            "Remote-Addr": "1.1.1.1",
+            "Authorization": "Basic dGVzdDp0ZXN0",
+            "Accept": "application/json"
+        }
+        response = self.request('/ip', headers=headers)
         self.assertEqual(response.status, '406 Not Acceptable')
 
     def test_ipRightHeader(self):
         """ /ip should prefer X-Real-Ip header to Remote-Addr header """
-        response = self.request('/ip', headers={"REMOTE-ADDR": "1.1.1.1", "X-REAL-IP": "2.2.2.2", "Authorization": "Basic dGVzdDp0ZXN0"})
-        result = response.collapse_body().decode('utf-8')
-        self.assertEqual(result, '2.2.2.2')
+        headers = {
+            "Remote-Addr": "1.1.1.1",
+            "X-REAL-IP": "2.2.2.2",
+            "Authorization": "Basic dGVzdDp0ZXN0"
+        }
+        response = self.request('/ip', headers=headers)
+        body = getResponseBody(response)
+        self.assertEqual(body, '2.2.2.2')
 
     def test_ipValidToken(self):
         """ /ip should return successfully if a valid token is specified """
-        response = self.request('/ip/test', headers={"REMOTE-ADDR": "1.1.1.1", "Authorization": "Basic dGVzdDp0ZXN0"})
+        headers = {
+            "Remote-Addr": "1.1.1.1",
+            "X-REAL-IP": "2.2.2.2",
+            "Authorization": "Basic dGVzdDp0ZXN0"
+        }
+        response = self.request('/ip/test', headers=headers)
         self.assertEqual(response.status, '200 OK')
 
     def test_ipInvalidToken(self):
         """ /ip should fail if an invalid token is specified """
-        response = self.request('/ip/invalid', headers={"REMOTE-ADDR": "1.1.1.1", "Authorization": "Basic dGVzdDp0ZXN0"})
+        headers = {
+            "Remote-Addr": "1.1.1.1",
+            "X-REAL-IP": "2.2.2.2",
+            "Authorization": "Basic dGVzdDp0ZXN0"
+        }
+        response = self.request('/ip/invalid', headers=headers)
         self.assertEqual(response.status, '404 Not Found')
 
     def test_ipNoIp(self):
         """ /ip should fail if it can't identify the request ip """
-        response = self.request('/ip/test', headers={"Authorization": "Basic dGVzdDp0ZXN0"})
+        headers = {
+            "Authorization": "Basic dGVzdDp0ZXN0"
+        }
+
+        response = self.request('/ip/test', headers=headers)
         self.assertEqual(response.status, '400 Bad Request')
 
 if __name__ == '__main__':
