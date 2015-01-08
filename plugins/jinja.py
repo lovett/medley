efbcff57a25eb4732d74f5509cdd6a9849529a3d
Bill Lovett <bill@ilovett.com>
2015-01-07 19:43:31 -0500

    Reorganize visitors template layout

--- plugins/jinja.py
+++ plugins/jinja.py
@@ -11,37 +11,31 @@ class Plugin(plugins.SimplePlugin):
 
         self.env.filters["datetime"] = self.datetime_filter
         self.env.filters["unindent"] = self.unindent_filter
+        self.env.filters["useragent"] = self.useragent_filter
 
         plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
-        """
-        Called when the engine starts.
-        """
+        """Called when the engine starts"""
         self.bus.log('Setting up Jinja2')
         self.bus.subscribe("lookup-template", self.get_template)
 
     def stop(self):
-        """
-        Called when the engine stops.
-        """
+        """Called when the engine stops"""
         self.bus.log('Freeing Jinja2 resources')
         self.bus.unsubscribe("lookup-template", self.get_template)
         self.env = None
 
     def get_template(self, name):
-        """
-        Returns Jinja2's template by name.
+        """Returns Jinja2's template by name
 
-        Used as follow:
+        Usage:
         >>> template = cherrypy.engine.publish('lookup-template', 'index.html').pop()
         """
         return self.env.get_template(name)
 
     def datetime_filter(self, value, format="locale"):
-        """
-        Format a datetime as a date string based on the specified format.
-        """
+        """Format a datetime as a date string based on the specified format"""
         if format == "locale":
             directives = "%c"
         elif format == "date-full":
@@ -59,3 +53,19 @@ class Plugin(plugins.SimplePlugin):
         indents.remove(0)
         unindented = [line.replace(" ", "", min(indents)) for line in lines]
         return "\n".join(unindented)
+
+    def useragent_filter(self, agent):
+        """Format the object returned by ua-parser into a string"""
+        out = ""
+
+        if agent["device"]["family"] == "Spider":
+            return agent["user_agent"]["family"]
+
+        family = agent["user_agent"]["family"]
+        version = agent["user_agent"]["major"]
+        os = agent["os"]["family"]
+
+        if family and version and os:
+            return "{} {}, {}".format(family, version, os)
+
+        return agent["string"]
