2a129302238dadf75d164ddfeaf023ddd4049239
Bill Lovett <bill@ilovett.com>
2015-05-16 21:01:13 -0400

    Add collapse all master toggle to visitors template

--- static/js/medley.js
+++ static/js/medley.js
@@ -3,9 +3,7 @@ var MEDLEY = (function () {
 
     var toggle = function (e) {
         e.preventDefault();
-        console.log('hi');
         var trigger = jQuery(this);
-        console.log(trigger);
         var target = trigger.nextAll('.toggle-target');
         var label = trigger.find('SPAN');
         target.toggleClass('hidden');
@@ -54,6 +52,18 @@ var MEDLEY = (function () {
         }
     };
 
+    function getMasterToggleForTable(table) {
+        var toggleId;
+        toggleId = jQuery(table).attr('id') + '-master-toggle';
+        return jQuery('#' + toggleId);
+    }
+
+    function getTableForMasterToggle(toggle) {
+        var tableId;
+        tableId = jQuery(toggle).attr('id').replace('-master-toggle', '');
+        return jQuery('#' + tableId);
+    }
+
     return {
         init: function () {
             jQuery('.toggle-trigger').on('click', toggle);
@@ -68,14 +78,73 @@ var MEDLEY = (function () {
 
             jQuery('.ui.radio.checkbox').checkbox();
 
-            jQuery('A.row-toggle').on('click', function (e) {
-                var label;
-                e.preventDefault();
-                label = jQuery('.label', this);
+            jQuery('TR.expandable').on('expand collapse', function (e) {
+                var prevRow, table, masterToggle;
 
-                label.text(label.text() === 'more' ? 'less':'more');
-                jQuery('.icon', this).toggleClass('cube cubes');
-                jQuery(this).closest('TR').next('TR.expandable').toggleClass('expanded');
+                prevRow = jQuery(this).prev('TR');
+                table = prevRow.closest('TABLE');
+
+                masterToggle = getMasterToggleForTable(table);
+
+                if (e.type === 'expand') {
+                    jQuery(this).addClass('expanded');
+                    prevRow.find('.toggle').trigger('expand');
+                } else if (e.type === 'collapse') {
+                    jQuery(this).removeClass('expanded');
+                    prevRow.find('.toggle').trigger('collapse');
+                }
+
+                jQuery('.master-toggle').trigger(e.type);
+            });
+
+            jQuery('.toggle').on('expand collapse', function (e) {
+                if (e.type === 'expand') {
+                    jQuery('.label', this).text('less');
+                    jQuery('.icon', this).removeClass('cubes').addClass('cube');
+                } else if (e.type === 'collapse') {
+                    jQuery('.label', this).text('more');
+                    jQuery('.icon', this).removeClass('cube').addClass('cubes');
+                }
+            });
+            
+            jQuery('A.toggle-next-row').on('click', function (e) {
+                var nextRow;
+                e.preventDefault();
+
+                nextRow = jQuery(this).closest('TR').next('TR.expandable');
+
+                if (nextRow.hasClass('expanded')) {
+                    nextRow.trigger('collapse');
+                } else {
+                    nextRow.trigger('expand');
+                }
+            });
+
+            jQuery('.master-toggle').on('click expand collapse', function (e) {
+                var label, table;
+                e.preventDefault();
+                
+                label = jQuery('.label', this);
+                table = getTableForMasterToggle(this);
+
+                if (e.type === 'expand') {
+                    label.text('collapse');
+                }
+                
+                if (e.type === 'collapse') {
+                    label.text('expand');
+                }
+
+                if (e.type === 'click') {
+                    if (label.text() === 'expand') {
+                        label.text('collapse');
+                        jQuery('TR.expandable', table).trigger('expand');
+                    } else {
+                        label.text('expand');
+                        jQuery('TR.expandable', table).trigger('collapse');
+                        return;
+                    }
+                }
             });
         }
     }
