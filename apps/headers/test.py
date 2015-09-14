bb880e6f2e567ca0c8589e38d5f713976b4a52a5
Bill Lovett <bill@ilovett.com>
2015-09-13 21:04:29 -0400

    Use helper methods for test server setup and app mounting
    By importing test helpers, an application's test suite can start up a
    CherryPy server with the specified application mounted as root. This
    should cut down on repetition across suites.

--- apps/headers/test.py
+++ apps/headers/test.py
@@ -1,43 +1,24 @@
-import sys
-sys.path.append("test")
-
-import unittest
-import cherrypy
-import apps.headers.main
-import plugins.jinja
 import cptestcase
+import helpers
+import unittest
+import apps.headers.main
 
 def setup_module():
-    cherrypy.config.update({
-        "app_roots": [],
-    })
-
-    app_config = {
-        "/": {
-            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
-            "tools.encode.on": False
-        }
-    }
-
-    app = cherrypy.tree.mount(apps.headers.main.Controller(), "/", app_config)
-
-    plugins.jinja.Plugin(cherrypy.engine).subscribe()
-    cherrypy.engine.start()
+    helpers.start_server(apps.headers.main.Controller)
 
 def teardown_module():
-    cherrypy.engine.exit()
+    helpers.stop_server()
 
 class TestHeaders(cptestcase.BaseCherryPyTestCase):
-
     def test_returnsHtml(self):
-        """ The application returns HTML by default """
+        """It returns HTML by default"""
         response = self.request("/")
         self.assertEqual(response.code, 200)
         self.assertEqual(response.headers["Content-Type"], "text/html;charset=utf-8")
         self.assertTrue("<table" in response.body)
 
     def test_returnsJson(self):
-        """ The application returns JSON if requested """
+        """It returns JSON if requested"""
         response = self.request("/", as_json=True)
         self.assertEqual(response.code, 200)
         self.assertEqual(response.headers["Content-Type"], "application/json")
@@ -45,29 +26,28 @@ class TestHeaders(cptestcase.BaseCherryPyTestCase):
         self.assertEqual(value, "application/json")
 
     def test_returnsPlain(self):
-        """ The application returns plain text if requested """
+        """It returns plain text if requested"""
         response = self.request("/", as_plain=True)
         self.assertEqual(response.code, 200)
         self.assertEqual(response.headers["Content-Type"], "text/plain;charset=utf-8")
         self.assertTrue("Accept" in response.body)
 
     def test_noVars(self):
-        """ The application takes no querystring arguments"""
+        """It takes no querystring arguments"""
         response = self.request("/?this=that")
         self.assertEqual(response.code, 404)
 
     def test_noParams(self):
-        """ The application takes no route parameters"""
+        """It takes no route parameters"""
         response = self.request("/test")
         self.assertEqual(response.code, 404)
 
     def test_customHeader(self):
-        """ The application recognizes custom headers"""
+        """It recognizes custom headers"""
         response = self.request("/", headers={"Special_Header": "Special Value"}, as_json=True)
         print(response.body)
         header, value = next(pair for pair in response.body if pair[0] == "Special_Header")
         self.assertEqual(value, "Special Value")
-
 
 
 if __name__ == "__main__":
--- test/helpers.py
+++ test/helpers.py
@@ -1,3 +1,27 @@
+import cherrypy
+import plugins.jinja
+
 def getFixture(path):
     with open("test/fixtures/" + path) as handle:
         return handle.read()
+
+def start_server(app):
+    cherrypy.config.update({
+        "app_roots": [],
+    })
+
+
+    app_config = {
+        "/": {
+            "request.dispatch": cherrypy.dispatch.MethodDispatcher(),
+            "tools.encode.on": False
+        }
+    }
+
+    cherrypy.tree.mount(app(), "/", app_config)
+
+    plugins.jinja.Plugin(cherrypy.engine).subscribe()
+    cherrypy.engine.start()
+
+def stop_server():
+    cherrypy.engine.exit()
