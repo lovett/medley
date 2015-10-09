7b2e366d63ba1cc97c46b99fbe1220bfc2f486e1
Bill Lovett <bill@ilovett.com>
2015-10-08 21:56:27 -0400

    Convert visitors endpoint to an app

---
+++ apps/visitors/main.py
@@ -0,0 +1,112 @@
+import sys
+import os.path
+sys.path.append("../../")
+
+import re
+import cherrypy
+import tools.negotiable
+import tools.jinja
+import requests
+import util.db
+import urllib.parse
+import apps.logindex.models
+import pycountry
+import pytz
+from datetime import datetime, timedelta
+
+class Controller:
+    """Search website access logs"""
+
+    exposed = True
+
+    user_facing = True
+
+    @cherrypy.tools.template(template="visitors.html")
+    @cherrypy.tools.negotiable()
+    def GET(self, q=None):
+
+        logman = apps.logindex.models.LogManager()
+
+        results = None
+        active_query = None
+
+        filters = {
+            "ip": [],
+            "include": [],
+            "exclude": [],
+            "shun": [],
+            "date":  []
+        }
+
+        saved_queries = util.db.getAnnotationsByPrefix("visitors")
+
+        if q:
+            for query in saved_queries:
+                if re.sub("\s", "", q, flags=re.UNICODE) == re.sub("\s", "", query["value"], flags=re.UNICODE):
+                    active_query = query["key"]
+                    break
+
+        if not q:
+            try:
+                active_query, q = [(query["key"], query["value"]) for query in saved_queries
+                     if query["key"] == "visitors:default"][0]
+            except IndexError:
+                q = ""
+
+
+        q = re.sub("[^\d\w -:;,\n]+", "", q, flags=re.UNICODE)
+        q = q.replace("date today", datetime.now().strftime("date %Y-%m-%d"))
+        q = q.replace("date yesterday", (datetime.now() - timedelta(days=1)).strftime("date %Y-%m-%d"))
+
+        if "," in q:
+            q = q.replace(",", "\n")
+
+        for line in q.split("\n"):
+            try:
+                subject, value = line.strip().split(" ", 1)
+                filters[subject].append(value)
+            except (ValueError, KeyError):
+                continue
+
+        log_dir = cherrypy.request.config.get("log_dir")
+
+        offsets = None
+
+        if len(filters["ip"]) > 0:
+            offsets = logman.getLogOffsets("ip", filters["ip"])
+            filters["date"] = offsets.keys()
+            del filters["ip"]
+
+
+        results, duration = util.fs.appengine_log_grep(log_dir, filters, offsets, 100)
+
+        for index, result in enumerate(results.matches):
+            needs_geo_lookup = "country" not in result
+            result["ip_facts"] = util.db.ipFacts(result["ip"], needs_geo_lookup)
+
+            if not needs_geo_lookup:
+                result["ip_facts"]["geo"]["country_code"] = result["country"]
+                result["ip_facts"]["geo"]["region_code"] = result["region"]
+                result["ip_facts"]["geo"]["city"] = result["city"]
+                if result["country"]:
+                    result["ip_facts"]["geo"]["country_name"] = pycountry.countries.get(alpha2=result["country"]).name
+                if "," in result.get("latlong"):
+                    (lat, lng) = result["latlong"].split(",")
+                    result["ip_facts"]["geo"]["latitude"] = lat
+                    result["ip_facts"]["geo"]["longitude"] = lng
+
+            try:
+                result["delta"] = result["timestamp"] - results.matches[index + 1]["timestamp"]
+            except IndexError:
+                result["delta"] = None
+
+        return {
+            "q": q,
+            "active_query": active_query,
+            "results": results.matches,
+            "total_matches": results.count,
+            "result_limit": results.limit,
+            "duration": duration,
+            "site_domains": cherrypy.request.config.get("site_domains"),
+            "saved_queries": saved_queries
+        }
--- medley.py
+++ medley.py
@@ -1,29 +1,16 @@
 import cherrypy
-import time
 import os.path
 import os
 import pwd
-import subprocess
-import re
-import IPy
 import json
 import plugins.jinja
-import pycountry
 import inspect
 import util.phone
 import util.net
 import util.html
-import util.fs
 import util.db
 import util.decorator
-import dogpile.cache
-import pytz
-import time
-import html.parser
 import syslog
-from collections import OrderedDict, defaultdict
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
 
 import apps.headers.main
 import apps.lettercase.main
@@ -39,6 +26,8 @@ import apps.later.main
 import apps.archive.main
 import apps.phone.main
 import apps.logindex.main
+import apps.visitors.main
+
 import apps.logindex.models
 
 import tools.negotiable
@@ -50,7 +39,6 @@ import tools.capture
 class MedleyServer(object):
     mc = None
     geoip = None
-    cache = dogpile.cache.make_region()
 
     def __init__(self):
         syslog.openlog(self.__class__.__name__)
@@ -59,8 +47,6 @@ class MedleyServer(object):
 
         util.db.setup(db_dir)
         util.db.geoSetup(db_dir, cherrypy.config.get("geoip.download.url"))
-
-        self.cache.configure_from_config(cherrypy.config, "cache.")
 
     @util.decorator.hideFromHomepage
     @cherrypy.expose
@@ -117,97 +103,6 @@ class MedleyServer(object):
         return {
             "q": q,
             "captures": util.db.getCaptures(q)
-        }
-
-    @cherrypy.expose
-    @cherrypy.tools.negotiable()
-    @cherrypy.tools.template(template="visitors.html")
-    def visitors(self, q=None):
-        """Search website access logs"""
-        logman = apps.logindex.models.LogManager()
-
-        results = None
-        active_query = None
-
-        filters = {
-            "ip": [],
-            "include": [],
-            "exclude": [],
-            "shun": [],
-            "date":  []
-        }
-
-        saved_queries = util.db.getAnnotationsByPrefix("visitors")
-
-        if q:
-            for query in saved_queries:
-                if re.sub("\s", "", q, flags=re.UNICODE) == re.sub("\s", "", query["value"], flags=re.UNICODE):
-                    active_query = query["key"]
-                    break
-
-        if not q:
-            try:
-                active_query, q = [(query["key"], query["value"]) for query in saved_queries
-                     if query["key"] == "visitors:default"][0]
-            except IndexError:
-                q = ""
-
-
-        q = re.sub("[^\d\w -:;,\n]+", "", q, flags=re.UNICODE)
-        q = q.replace("date today", datetime.now().strftime("date %Y-%m-%d"))
-        q = q.replace("date yesterday", (datetime.now() - timedelta(days=1)).strftime("date %Y-%m-%d"))
-
-        if "," in q:
-            q = q.replace(",", "\n")
-
-        for line in q.split("\n"):
-            try:
-                subject, value = line.strip().split(" ", 1)
-                filters[subject].append(value)
-            except (ValueError, KeyError):
-                continue
-
-        log_dir = cherrypy.request.config.get("log_dir")
-
-        offsets = None
-
-        if len(filters["ip"]) > 0:
-            offsets = logman.getLogOffsets("ip", filters["ip"])
-            filters["date"] = offsets.keys()
-            del filters["ip"]
-
-
-        results, duration = util.fs.appengine_log_grep(log_dir, filters, offsets, 100)
-
-        for index, result in enumerate(results.matches):
-            needs_geo_lookup = "country" not in result
-            result["ip_facts"] = util.db.ipFacts(result["ip"], needs_geo_lookup)
-
-            if not needs_geo_lookup:
-                result["ip_facts"]["geo"]["country_code"] = result["country"]
-                result["ip_facts"]["geo"]["region_code"] = result["region"]
-                result["ip_facts"]["geo"]["city"] = result["city"]
-                if result["country"]:
-                    result["ip_facts"]["geo"]["country_name"] = pycountry.countries.get(alpha2=result["country"]).name
-                if "," in result.get("latlong"):
-                    (lat, lng) = result["latlong"].split(",")
-                    result["ip_facts"]["geo"]["latitude"] = lat
-                    result["ip_facts"]["geo"]["longitude"] = lng
-
-            try:
-                result["delta"] = result["timestamp"] - results.matches[index + 1]["timestamp"]
-            except IndexError:
-                result["delta"] = None
-
-        return {
-            "q": q,
-            "active_query": active_query,
-            "results": results.matches,
-            "total_matches": results.count,
-            "result_limit": results.limit,
-            "duration": duration,
-            "site_domains": cherrypy.request.config.get("site_domains"),
-            "saved_queries": saved_queries
         }
 
 
