2cf1ec335070796cb2fa82dfdae2c456ea908a31
Bill Lovett <bill@ilovett.com>
2016-02-10 23:50:14 -0500

    Add a top-level bash script for setting up a tmux session

---
+++ workspace.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -u
+
+PROJECT_ROOT=$(pwd)
+PROJECT_NAME=$(basename "$PROJECT_ROOT")
+
+# Start the server if not already running
+tmux start-server 2> /dev/null
+
+# Connect to a session or create a new one
+tmux attach-session -d -t "$PROJECT_NAME" || {
+    echo "Creating a new session"
+
+    ## 0: Editor
+    tmux new-session -d -s "$PROJECT_NAME" bash
+    tmux send-keys -t "$PROJECT_NAME" "e ." C-m
+
+    ## 1: Shell
+    tmux new-window -a -t "$PROJECT_NAME" bash
+
+    ## 2: Dev server
+    tmux new-window -a -t "$PROJECT_NAME" -n "devserver" "source venv/bin/activate; python medley.py"
+
+    tmux select-window -t "$PROJECT_NAME":0
+
+    tmux attach-session -t "$PROJECT_NAME"
+}
