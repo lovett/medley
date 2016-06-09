d7eabdaf3b92ed49a02c5fcaa64178c64b7a5f30
Bill Lovett <bill@ilovett.com>
2016-06-08 22:22:01 -0400

    [azure] Round out test coverage

--- apps/azure/test.py
+++ apps/azure/test.py
@@ -33,12 +33,27 @@ class TestAzure(cptestcase.BaseCherryPyTestCase):
         })
         self.assertEqual(response.code, 400)
 
+    @mock.patch("apps.registry.models.Registry.search")
+    def test_needsNotifierConfig(self, registrySearchMock):
+        """An error is raised the registry does not have notifier credentials"""
+        registrySearchMock.side_effect = [
+            [], []
+        ]
+        response = self.request("/", method="POST", json_body={
+            "status": "success",
+            "siteName": self.site_name
+        })
+        self.assertEqual(response.code, 500)
+
     @requests_mock.Mocker()
     @mock.patch("apps.registry.models.Registry.search")
     def test_sendsSuccessNotification(self, requestsMock, registrySearchMock):
-        """A status of failed triggers a failure notification"""
+        """A success status triggers a success notification"""
 
-        registrySearchMock.return_value = self.notifier_config
+        registrySearchMock.side_effect = [
+            self.notifier_config,
+            []
+        ]
 
         requestsMock.register_uri("POST", "http://example.com")
 
@@ -60,9 +75,12 @@ class TestAzure(cptestcase.BaseCherryPyTestCase):
     @requests_mock.Mocker()
     @mock.patch("apps.registry.models.Registry.search")
     def test_sendsFailureNotification(self, requestsMock, registrySearchMock):
-        """A status of failed triggers a failure notification"""
+        """A failed status triggers a failure notification"""
 
-        registrySearchMock.return_value = self.notifier_config
+        registrySearchMock.side_effect = [
+            self.notifier_config,
+            []
+        ]
 
         requestsMock.register_uri("POST", "http://example.com")
 
@@ -87,7 +105,9 @@ class TestAzure(cptestcase.BaseCherryPyTestCase):
     def test_sendsUnknownNotification(self, requestsMock, registrySearchMock):
         """An unknown status value triggers a notification"""
 
-        registrySearchMock.return_value = self.notifier_config
+        registrySearchMock.side_effect = [
+            self.notifier_config, []
+        ]
 
         requestsMock.register_uri("POST", "http://example.com")
 
@@ -105,6 +125,34 @@ class TestAzure(cptestcase.BaseCherryPyTestCase):
         self.assertFalse("has+failed" in notification.text)
         self.assertFalse("is+complete" in notification.text)
         self.assertTrue(self.site_name in notification.text)
+
+
+    @requests_mock.Mocker()
+    @mock.patch("apps.registry.models.Registry.search")
+    def test_siteNamePopulatedInAzurePortalUrl(self, requestsMock, registrySearchMock):
+        """The site name is populated within the Azure portal url"""
+
+        registrySearchMock.side_effect = [
+            self.notifier_config,
+            [{"key": "azure:portal_url", "value": "http://test.example.com/{}/portal"}]
+        ]
+
+        requestsMock.register_uri("POST", "http://example.com")
+
+        response = self.request("/", method="POST", json_body={
+            "siteName": self.site_name,
+            "status": "success",
+            "complete": True
+        })
+
+        notification = requestsMock.request_history[0]
+
+        self.assertEqual(response.code, 200)
+        self.assertTrue(requestsMock.called)
+        self.assertTrue("url=" in notification.text)
+        self.assertTrue(self.site_name in notification.text)
+
+
 
 
 
