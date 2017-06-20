54afb9ba9985be55c0eff6f1b18fc53c80ee2e96
Bill Lovett <bill@ilovett.com>
2017-06-20 15:43:04 -0400

    [jenkins] Rework notification title and body
    Be more terse, and vary the format depending on the circumstances of
    the build.

--- apps/jenkins/main.py
+++ apps/jenkins/main.py
@@ -5,7 +5,23 @@ import apps.registry.models
 import tools.capture
 
 class Controller:
-    """Relay deployment notifications from Jenkins"""
+    """Relay deployment notifications from Jenkins
+
+    This works with the Jenkins Notification Plugin:
+    https://wiki.jenkins-ci.org/display/JENKINS/Notification+Plugin
+
+    Each Jenkins job should specify this app's URL as a notification
+    endpoint. All event types will be accepted, but the completed event
+    will be silently dropped in favor of the finalized event to avoid
+    double notification.
+
+    The difference between the finalized and completed events is
+    described at the URL above: "...[W]hen job is finalized all
+    post-build activities, such as archiving artifacts, were executed
+    as well. This is not the case with job being merely "completed"
+    which usually involves only creation of job's artifacts without
+    post-processing them."
+    """
 
     name = "Jenkins"
 
@@ -26,7 +42,10 @@ class Controller:
 
         notifier_config = registry.search(key="notifier:*")
         if not notifier_config:
-            raise cherrypy.HTTPError(500, "No notification config found in registry")
+            raise cherrypy.HTTPError(
+                500,
+                "No notification config found in registry"
+            )
 
         notifier = {}
         for item in notifier_config:
@@ -38,9 +57,42 @@ class Controller:
             "url": details["build"]["full_url"]
         }
 
-        # Phase can be STARTED, COMPLETED, or FINALIZED
-        notification["title"] = "Jenkins build {} has {}".format(details["name"], details["build"]["phase"].lower())
-        notification["body"] = "Status: {}".format(details["build"]["status"])
+        if details["build"]["log"]:
+            notification["body"] = details["build"]["log"].split()[0]
 
-        r = requests.post(notifier["url"], auth=(notifier["username"], notifier["password"]), data=notification)
+        if details["build"]["phase"].lower() == "completed":
+            # Disregard completed events to avoid double-notification with
+            # finalized event (Jenkins is expected to send all supported
+            # event types).
+            cherrypy.response.status = 204
+            return
+
+        if details["build"]["phase"].lower() == "started":
+            notification["title"] = "Starting a build for {}".format(details["name"])
+            notification["body"] = "Build #{}".format(details["build"]["number"])
+
+
+        if details["build"]["phase"].lower() == "finalized":
+            if details["build"]["status"].lower() == "success":
+                notification["title"] = "{} #{} has {}".format(
+                    details["name"],
+                    details["build"]["number"],
+                    details["build"]["phase"].lower()
+                )
+            else:
+                notification["title"] = "{} #{} had trouble".format(
+                    details["name"],
+                    details["build"]["number"]
+                )
+
+                notification["body"] = "Status: {}".format(
+                    details["build"]["status"]
+                )
+
+        r = requests.post(
+            notifier["url"],
+            auth=(notifier["username"], notifier["password"]),
+            data=notification
+        )
+
         r.raise_for_status()
