ef825bb68b51ea4ac6668926ecd58db671ebe4ab
Bill Lovett <bill@ilovett.com>
2017-06-24 22:46:07 -0400

    [notification] Add app for receiving notification webhoook requests

---
+++ apps/notification/main.py
@@ -0,0 +1,31 @@
+import cherrypy
+import apps.speak.models
+import tools.capture
+
+class Controller:
+    """
+    Relay messages from notifier to the speak app
+    """
+
+    name = "Notification"
+
+    _cp_config = {
+        'tools.conditional_auth.on': False
+    }
+
+    exposed = True
+
+    user_facing = False
+
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.capture()
+    def POST(self):
+        notification = cherrypy.request.json
+        print(notification)
+        if not notification["title"]:
+            cherrypy.response.status = 400
+
+        manager = apps.speak.models.SpeechManager()
+        manager.say(notification["title"], "en-GB", "Male")
+
+        return
