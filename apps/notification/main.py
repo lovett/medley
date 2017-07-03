711c4961bd636d528416e900cb1ccfaf7a01ae84
Bill Lovett <bill@ilovett.com>
2017-07-03 17:01:29 -0400

    [notification] Answer retraction messages with a 204

--- apps/notification/main.py
+++ apps/notification/main.py
@@ -21,8 +21,16 @@ class Controller:
     @cherrypy.tools.capture()
     def POST(self):
         notification = cherrypy.request.json
-        if not "title" in notification:
+
+        if "retracted" in notification:
+            cherrypy.response.status = 204
+            return
+
+        title = notification.get("title")
+
+        if not title:
             cherrypy.response.status = 400
+            return
 
         manager = apps.speak.models.SpeechManager()
         manager.say(notification["title"], "en-GB", "Male")
