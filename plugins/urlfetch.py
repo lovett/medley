921ecdb6380255ea0d153d5cc7b6d10e06fecba0
Bill Lovett <bill@ilovett.com>
2017-08-30 21:45:46 -0400

    Narrow responsibility of urlfetch to just url fetching
    Leave caching up to the caller rather than incorporate it into the
    fetching process. Although convenient, this makes it harder to keep
    track of what is going on.

--- plugins/urlfetch.py
+++ plugins/urlfetch.py
@@ -12,42 +12,28 @@ class Plugin(plugins.SimplePlugin):
     def stop(self):
         pass
 
-    def get(self, url, headers={}, timeout=5, as_json=False, cache_key=None):
+    def get(self, url, headers={}, timeout=5, as_json=False):
         """Request the specified URL"""
 
-        if not cache_key:
-            cache_key = "{}::{}".format(url, int(as_json))
-
-        cached_value = cherrypy.engine.publish("cache:get", cache_key).pop()
-
-        if cached_value:
-            print("cached!")
-            return cached_value
-
-        print("not cached")
-
-        fetch_headers = {
+        request_headers = {
             "User-Agent": "python",
         }
 
-        fetch_headers.update(headers)
+        request_headers.update(headers)
 
         try:
             req = requests.get(
                 url,
                 timeout=timeout,
-                headers=fetch_headers
+                headers=request_headers
             )
 
             req.raise_for_status()
 
-            result = req.text
-
             if as_json:
-                result = req.json()
+                return req.json()
 
-            cherrypy.engine.publish("cache:set", cache_key, result)
-            return result
+            return req.text
 
         except requests.exceptions.RequestException:
             return None
