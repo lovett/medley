63782d6287438d6b7015bf25b887583e1e793e4c
Bill Lovett <bill@ilovett.com>
2017-08-31 23:50:07 -0400

    [grids] Use registry plugin instead of model class

--- apps/grids/main.py
+++ apps/grids/main.py
@@ -1,9 +1,7 @@
 import cherrypy
-import apps.registry.models
 from collections import defaultdict
 import datetime
-import calendar
-
+from calendar import Calendar
 
 class Controller:
     """Printable pages for data entry"""
@@ -18,12 +16,16 @@ class Controller:
 
     @cherrypy.tools.negotiable()
     def GET(self, name="", start=None):
-        registry = apps.registry.models.Registry()
+        grids = cherrypy.engine.publish(
+            "registry:search",
+            "grids:*",
+            as_dict=True
+        ).pop()
 
-        grids = registry.search(key="grids:*")
-        names = [grid["key"].split(":")[1] for grid in grids]
         options = defaultdict(lambda: None)
+
         today = datetime.date.today()
+
         try:
             start = datetime.datetime.strptime(start, "%Y-%m")
         except (TypeError, ValueError):
@@ -31,17 +33,16 @@ class Controller:
 
         try:
             config = next(
-                grid["value"].split("\n")
-                for grid in grids if grid["key"].endswith(":" + name)
+                value.split("\n")
+                for key, value in grids.items() if key.endswith(":" + name)
             )
-            headers = [
-                value.strip()
-                for value in config[0].split(",")
-            ]
+
+            headers = [value.strip() for value in config[0].split(",")]
+
             options.update([value.split('=') for value in config[1:]])
+
         except StopIteration:
             headers = []
-
 
         rows = []
         if options["layout"] == "month":
@@ -49,8 +50,7 @@ class Controller:
             options["last_month"] = start - datetime.timedelta(days=1)
             options["next_month"] = start + datetime.timedelta(days=32)
             options["this_month"] = today
-            cal = calendar.Calendar()
-            for item in cal.itermonthdates(start.year, start.month):
+            for item in Calendar().itermonthdates(start.year, start.month):
                 if item.month != start.month:
                     continue
                 row = [''] * len(headers)
@@ -63,12 +63,11 @@ class Controller:
 
         return {
             "html": ("grids.html", {
-                "headers": headers,
                 "app_name": self.name,
-                "names": names,
-                "name": name,
                 "headers": headers,
+                "name": name,
+                "names": [key.split(":").pop() for key in grids.keys()],
+                "options": options,
                 "rows": rows,
-                "options": options
             })
         }
