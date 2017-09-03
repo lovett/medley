3f0360b70d4884ee45caae7fd1b4287920844b13
Bill Lovett <bill@ilovett.com>
2017-09-02 23:04:28 -0400

    [grids] Add tests

--- apps/grids/main.py
+++ apps/grids/main.py
@@ -22,14 +22,8 @@ class Controller:
             as_dict=True
         ).pop()
 
+
         options = defaultdict(lambda: None)
-
-        today = datetime.date.today()
-
-        try:
-            start = datetime.datetime.strptime(start, "%Y-%m")
-        except (TypeError, ValueError):
-            start = today.replace(day=1)
 
         try:
             config = next(
@@ -46,6 +40,13 @@ class Controller:
 
         rows = []
         if options["layout"] == "month":
+            today = datetime.date.today()
+
+            try:
+                start = datetime.datetime.strptime(start, "%Y-%m")
+            except (TypeError, ValueError):
+                start = today.replace(day=1)
+
             headers = ["Date", "Day"] + headers
             options["last_month"] = start - datetime.timedelta(days=1)
             options["next_month"] = start + datetime.timedelta(days=32)
@@ -57,7 +58,7 @@ class Controller:
                 row[0] = item.strftime("%B %d, %Y")
                 row[1] = item.strftime("%A")
                 rows.append(row)
-        else:
+        elif headers:
             row = [''] * len(headers)
             rows = [row for x in range(1, 30)]
 
@@ -66,7 +67,7 @@ class Controller:
                 "app_name": self.name,
                 "headers": headers,
                 "name": name,
-                "names": [key.split(":").pop() for key in grids.keys()],
+                "names": [key.split(":")[1] for key in grids.keys()],
                 "options": options,
                 "rows": rows,
             })
--- apps/grids/test_main.py
+++ apps/grids/test_main.py
@@ -1,10 +1,27 @@
+from testing import assertions
 from testing import cptestcase
 from testing import helpers
-import unittest
 import apps.grids.main
+import cherrypy
+import datetime
+import mock
+import unittest
 
+class TestGrids(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
+    """Unit tests for the Grids app controller
 
-class TestGrids(cptestcase.BaseCherryPyTestCase):
+    Testing involves the use of two mocks. The first mock patches
+    cherrypy.engine.publish in order to inject a fixture for the grid
+    template.
+
+    The controller is not the only thing calling this method. If the
+    mock makes it as far as the template plugin, the request will fail
+    due to lack of an appropriate return value (side effect).
+
+    The second mock against cherrpy.tools.negotiable avoids this. It
+    provides a window into the dict returned by the controller before
+    the templating plugin gets involved."""
+
     @classmethod
     def setUpClass(cls):
         helpers.start_server(apps.grids.main.Controller)
@@ -13,10 +30,89 @@ class TestGrids(cptestcase.BaseCherryPyTestCase):
     def tearDownClass(cls):
         helpers.stop_server()
 
-    def test_returnsHtml(self):
-        """It returns HTML by default"""
+    def extract_template_vars(self, mock):
+        return mock.call_args[0][0]["html"][1]
+
+    @mock.patch("cherrypy.tools.negotiable._renderHtml")
+    @mock.patch("cherrypy.engine.publish")
+    def test_monthLayout(self, publishMock, renderMock):
+        """The first two columns of a template with layout=month are Date and
+        Day, even though these are not otherwise specified in the template"""
+
+        def side_effect(*args, **kwargs):
+            if (args[0] == "registry:search"):
+                return [{"grids:test1": "Column 1, Column 2, Column 3\nlayout=month"}]
+
+        publishMock.side_effect = side_effect
+
+        response = self.request("/", name="test1")
+
+        template_vars = self.extract_template_vars(renderMock)
+
+        self.assertCountEqual(
+            template_vars["headers"],
+            ['Date', 'Day', 'Column 1', 'Column 2', 'Column 3']
+        )
+
+    @mock.patch("cherrypy.tools.negotiable._renderHtml")
+    @mock.patch("cherrypy.engine.publish")
+    def test_monthLayoutInvalidStart(self, publishMock, renderMock):
+        """An invalid start date for a monthly layout is handled gracefully"""
+
+        first_of_current_month = datetime.date.today().replace(day=1)
+
+        def side_effect(*args, **kwargs):
+            if (args[0] == "registry:search"):
+                return [{"grids:test1": "Column 4, Column 5, Column 6\nlayout=month"}]
+
+        publishMock.side_effect = side_effect
+
+        response = self.request("/", name="test1", start="1234-56")
+
+        template_vars = self.extract_template_vars(renderMock)
+
+        self.assertEqual(
+            template_vars["rows"][0][0],
+            first_of_current_month.strftime("%B %d, %Y")
+        )
+
+
+    @mock.patch("cherrypy.tools.negotiable._renderHtml")
+    @mock.patch("cherrypy.engine.publish")
+    def test_plainLayout(self, publishMock, renderMock):
+        """No additional columns are added to a plain-layout template"""
+
+        def side_effect(*args, **kwargs):
+            if (args[0] == "registry:search"):
+                return [{"grids:test1": "Column A, Column B"}]
+
+        publishMock.side_effect = side_effect
+
+        response = self.request("/", name="test1")
+
+        template_vars = self.extract_template_vars(renderMock)
+
+        self.assertCountEqual(template_vars["headers"], ['Column A', 'Column B'])
+
+    @mock.patch("cherrypy.tools.negotiable._renderHtml")
+    @mock.patch("cherrypy.engine.publish")
+    def test_defaultView(self, publishMock, renderMock):
+        """The default view of the app is a list of available templates"""
+        def side_effect(*args, **kwargs):
+            if (args[0] == "registry:search"):
+                return [{"grids:test1": "Column A"}]
+
+        publishMock.side_effect = side_effect
+
         response = self.request("/")
-        self.assertEqual(response.code, 200)
+
+        template_vars = self.extract_template_vars(renderMock)
+
+        print(template_vars)
+
+        self.assertFalse(template_vars["rows"])
+        self.assertFalse(template_vars["headers"])
+        self.assertEqual(template_vars["names"], ["test1"])
 
 if __name__ == "__main__":
     unittest.main()
