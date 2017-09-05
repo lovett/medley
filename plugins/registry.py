cff1ead8482355a7d18f4f0ae65f481f6b20fa20
Bill Lovett <bill@ilovett.com>
2017-09-04 22:16:10 -0400

    [plugins] Migrate search method from registry model to plugin

--- plugins/registry.py
+++ plugins/registry.py
@@ -18,6 +18,7 @@ class Plugin(cherrypy.process.plugins.SimplePlugin, mixins.Sqlite):
     def start(self):
         self.bus.subscribe("registry:remove", self.remove)
         self.bus.subscribe("registry:add", self.add)
+        self.bus.subscribe("registry:search", self.search)
 
     def stop(self):
         pass
@@ -30,6 +31,54 @@ class Plugin(cherrypy.process.plugins.SimplePlugin, mixins.Sqlite):
             "INSERT INTO registry (key, value) VALUES (?, ?)",
              [(key, value) for value in values]
         )
+
+    def search(self, key=None, keys=[], value=None, limit=100, exact=False, as_dict=False, as_value_list=False):
+        params = []
+
+        sql = "SELECT rowid, key, value, created as 'created [created]' FROM registry WHERE (1) "
+
+        if len(keys) > 0:
+            sql += "AND key IN ("
+            sql += ", ".join("?" * len(keys))
+            sql += ") "
+        elif key:
+            fuzzy = "*" in key
+
+            if fuzzy:
+                key = key.replace("*", "%")
+            elif not exact:
+                key = "%{}%".format(key)
+
+            if exact:
+                sql += "AND key = ?"
+            else:
+                sql += "AND key LIKE ? "
+
+            params.append(key)
+
+        if value:
+            fuzzy = "*" in value
+            value = value.replace("*", "%")
+
+            if fuzzy:
+                sql += "AND VALUE LIKE ?"
+            else:
+                sql += "AND value=?"
+
+            params.append(value)
+
+        sql += " ORDER BY rowid DESC"
+        sql += " LIMIT {}".format(limit)
+
+        result = self._select(sql, params)
+
+        if as_dict:
+            result = {row["key"]: row["value"] for row in result}
+
+        if as_value_list:
+            result = [row["value"] for row in result]
+
+        return result
 
     def remove(self, key):
         deletions = self._delete("DELETE FROM registry WHERE key=?", (key,))
