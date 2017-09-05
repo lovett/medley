6a8c02c78340e5a5faa41fb0359fe3d7018d7263
Bill Lovett <bill@ilovett.com>
2017-09-04 22:18:02 -0400

    [plugins] Add support for POST requests

--- plugins/urlfetch.py
+++ plugins/urlfetch.py
@@ -1,5 +1,6 @@
 import cherrypy
 import requests
+import json
 from cherrypy.process import plugins
 
 class Plugin(plugins.SimplePlugin):
@@ -8,12 +9,13 @@ class Plugin(plugins.SimplePlugin):
 
     def start(self):
         self.bus.subscribe("urlfetch:get", self.get)
+        self.bus.subscribe("urlfetch:post", self.post)
 
     def stop(self):
         pass
 
-    def get(self, url, headers={}, timeout=5, as_json=False):
-        """Request the specified URL"""
+    def get(self, url, auth=(), headers={}, timeout=5, as_json=False):
+        """Send a GET request"""
 
         request_headers = {
             "User-Agent": "python",
@@ -24,6 +26,7 @@ class Plugin(plugins.SimplePlugin):
         try:
             req = requests.get(
                 url,
+                auth=auth,
                 timeout=timeout,
                 headers=request_headers
             )
@@ -34,6 +37,35 @@ class Plugin(plugins.SimplePlugin):
                 return req.json()
 
             return req.text
+
+        except requests.exceptions.RequestException:
+            return None
+
+    def post(self, url, data, auth=(), headers={}, timeout=5, as_json=False):
+        """Send a POST request"""
+
+        request_headers = {
+            "User-Agent": "python",
+        }
+
+        request_headers.update(headers)
+
+        if (as_json):
+            data=json.dumps(payload)
+            request_headers["Content-Type"] = "application/json"
+
+        try:
+            req = requests.post(
+                url,
+                auth=auth,
+                timeout=timeout,
+                headers=request_headers,
+                data=data
+            )
+
+            req.raise_for_status()
+
+            return req.json() if as_json else req.text
 
         except requests.exceptions.RequestException:
             return None
