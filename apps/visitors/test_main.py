209e21b342c5e8c08aaf7bff5bdb740494a04bf3
Bill Lovett <bill@ilovett.com>
2017-11-29 22:02:01 -0500

    [visitors] Start refactoring of controller tests

--- apps/visitors/test_main.py
+++ apps/visitors/test_main.py
@@ -1,20 +1,11 @@
+from testing import assertions
 from testing import cptestcase
 from testing import helpers
-import cherrypy
-import pytest
 import unittest
-import responses
 import apps.visitors.main
-import apps.logindex.models
-import apps.registry.models
 import mock
-import tempfile
-import shutil
-import datetime
-import util.fs
-import util.ip
 
-class TestTopics(cptestcase.BaseCherryPyTestCase):
+class TestVisitors(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
     @classmethod
     def setUpClass(cls):
         helpers.start_server(apps.visitors.main.Controller)
@@ -23,161 +14,25 @@ class TestTopics(cptestcase.BaseCherryPyTestCase):
     def tearDownClass(cls):
         helpers.stop_server()
 
-    def setUp(self):
-        self.temp_dir = tempfile.mkdtemp(prefix="visitors-test")
-        cherrypy.config["database_dir"] = self.temp_dir
-        cherrypy.config["log_dir"] = self.temp_dir
 
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+    def extract_template_vars(self, mock, media="html"):
+        return mock.call_args[0][0][media][1]
 
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry")
-    def test_returnsHtml(self, registryMock, logManagerMock):
-        """It returns HTML"""
+    def test_allow(self):
+        """The app supports GET, PUT, and DELETE operations"""
+        response = self.request("/", method="HEAD")
+        self.assertAllowedMethods(response, ("GET",))
+
+    @mock.patch("cherrypy.tools.negotiable._renderHtml")
+    @mock.patch("cherrypy.engine.publish")
+    def test_noRoot(self, publishMock, renderMock):
+        def side_effect(*args, **kwargs):
+            if args[0] == "registry:first_value" and args[1] == "logindex:root":
+                return [None]
+
+        publishMock.side_effect = side_effect
         response = self.request("/")
-        self.assertEqual(response.code, 200)
-        self.assertTrue(helpers.response_is_html(response))
-
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_retrievesSavedQueries(self, registrySearchMock):
-        """Previously-saved queries are retrieved from the registry"""
-        response = self.request("/")
-        self.assertTrue(registrySearchMock.called)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_identifiesActiveQuery(self, registrySearchMock, logManagerMock):
-        """The saved query dropdown pre-selects the current query"""
-        registrySearchMock.return_value = [
-            {"key": "key2", "value": "value 2"},
-            {"key": "key1", "value": "value 1"}
-        ]
-        response = self.request("/", q="value 1")
-        self.assertTrue("""selected="selected">key1</option>""" in response.body)
-
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_identifiesNoActiveQuery(self, registrySearchMock, logManagerMock):
-        """No query is pre-selected if there are no saved queries"""
-        registrySearchMock.return_value = []
-        response = self.request("/", q="value 1")
-        self.assertFalse("""selected="selected">""" in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_identifiesDefaultQuery(self, registrySearchMock, logManagerMock):
-        """The default query is pre-selected if available"""
-        registrySearchMock.return_value = [
-            {"key": "visitors:default", "value": "value 2"},
-            {"key": "visitors:default", "value": "value 1"}
-        ]
-        response = self.request("/")
-        self.assertTrue("""selected="selected">visitors:default</option>""" in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_convertsDateToday(self, registrySearchMock, logManagerMock):
-        """If the query references the current date, it is calculated"""
-        registrySearchMock.return_value = []
-        response = self.request("/", q="date today")
-        today = datetime.datetime.now().strftime("%Y-%m-%d")
-        self.assertTrue(">date {}</textarea>".format(today) in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_convertsDateYesterday(self, registrySearchMock, logManagerMock):
-        """If the query references the current date, it is calculated"""
-        registrySearchMock.return_value = []
-        response = self.request("/", q="date yesterday")
-        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-        self.assertTrue(">date {}</textarea>".format(yesterday) in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_sanitizesQuery(self, registrySearchMock, logManagerMock):
-        """Queries are restricted to alphanumerics and limited punctuation"""
-        registrySearchMock.return_value = []
-        response = self.request("/", q="test ★")
-        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-        self.assertTrue(">test</textarea>".format(yesterday) in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("apps.logindex.models.LogManager")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_producesMultilineQuery(self, registrySearchMock, logManagerMock):
-        """Commas within queries are interpreted as clause separators, and replaced with newlines"""
-        registrySearchMock.return_value = []
-        response = self.request("/", q="line1, line2, line3")
-        self.assertTrue(">line1\nline2\nline3</textarea>" in response.body)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("util.fs.appengine_log_grep")
-    @mock.patch("apps.logindex.models.LogManager.getLogOffsets")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_filtersByDate(self, registrySearchMock, offsetsMock, grepMock):
-        """IP filters are used as log index keys"""
-        registrySearchMock.return_value = []
-        offsetsMock.return_value = {"2015-01-01": [], "2015-01-02":[]}
-        response = self.request("/", q="ip 1.2.3.4")
-        filters = grepMock.call_args[0][1]
-        self.assertTrue("ip" not in filters)
-        self.assertTrue("2015-01-01" in filters["date"])
-        self.assertTrue("2015-01-02" in filters["date"])
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("util.ip.facts")
-    @mock.patch("util.fs.appengine_log_grep")
-    @mock.patch("apps.logindex.models.LogManager.getLogOffsets")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_skipsGeoipLookup(self, registrySearchMock, offsetsMock, grepMock, ipFactsMock):
-        """A GeoIP lookup does not occur if the log line has geoip fields"""
-        registrySearchMock.return_value = []
-        grep_result = util.fs.GrepResult([
-            {
-                "ip": "1.2.3.4",
-                "country": "US",
-                "statusCode": 200,
-                "latlong": "1,2"
-            }
-        ], 1, 100)
-        grepMock.return_value = (grep_result, 1)
-        response = self.request("/", q="ip 1.2.3.4")
-        self.assertTrue(grepMock.called)
-        self.assertTrue(offsetsMock.called)
-        self.assertFalse(ipFactsMock.called)
-        self.assertEqual(response.code, 200)
-
-    @pytest.mark.skip(reason="pending refactor")
-    @mock.patch("util.ip.facts")
-    @mock.patch("util.fs.appengine_log_grep")
-    @mock.patch("apps.logindex.models.LogManager.getLogOffsets")
-    @mock.patch("apps.registry.models.Registry.search")
-    def test_populatesGeoIpFromLookup(self, registrySearchMock, offsetsMock, grepMock, ipFactsMock):
-        """A GeoIP lookup occurs if the log line does not include geoip fields"""
-        registrySearchMock.return_value = []
-        grep_result = util.fs.GrepResult([
-            {
-                "ip": "1.2.3.4",
-                "statusCode": 200
-            }
-        ], 1, 100)
-        ipFactsMock.return_value = {
-            "geo": {}
-        }
-        grepMock.return_value = (grep_result, 1)
-        response = self.request("/", q="ip 1.2.3.4")
-        self.assertTrue(grepMock.called)
-        self.assertTrue(offsetsMock.called)
-        self.assertTrue(ipFactsMock.called)
-        self.assertEqual(response.code, 200)
-
-
+        self.assertEqual(response.code, 500)
 
 if __name__ == "__main__":
     unittest.main()
