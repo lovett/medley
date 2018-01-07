9ad93a22d69c95972b1bc04328a52481e7c46a9c
Bill Lovett <bill@ilovett.com>
2018-01-07 15:36:46 -0500

    Specify whitelisted IPs for conditional auth as a string
    Although they can be a tuple, you have to be extra careful to use a
    trailing comma if only one value is specified. Having to remember this
    is annoying. Use a space delimited string instead.
    
    The conditional_auth tool is also reworked to be slightly more readable.

--- tools/conditional_auth.py
+++ tools/conditional_auth.py
@@ -2,18 +2,29 @@ import cherrypy
 
 class Tool(cherrypy.Tool):
     """Prompt for basic authentication unless the request originates from a
-    whitelisted IP"""
+    whitelisted IP
+
+    Whitelisted IPs should be specified in the global application
+    config under the key tools.conditional_auth.whitelist
+    """
 
     def __init__(self):
-        cherrypy.Tool.__init__(self, 'on_start_resource',
-                               self._noauth, priority=1)
+        cherrypy.Tool.__init__(
+            self,
+            "on_start_resource",
+            self._auth,
+            priority=1
+        )
 
     def _checkpassword(self, realm, user, password):
         users = cherrypy.config.get("users")
         p = users.get(user)
         return p and p == password or False
 
-    def _noauth(self, whitelist=()):
+    def _auth(self, whitelist=None):
+
+        ips = whitelist.split()
+        ips.append("127.0.0")
 
         address = None
         for header in ("X-Real-Ip", "Remote-Addr"):
@@ -23,7 +34,9 @@ class Tool(cherrypy.Tool):
             except KeyError:
                 pass
 
-        if not any(address.startswith(item) for item in whitelist):
-            cherrypy.tools.auth_basic.callable(realm="medley", checkpassword=self._checkpassword)
+        if any(address.startswith(ip) for ip in ips):
+            return
+
+        cherrypy.tools.auth_basic.callable(realm="medley", checkpassword=self._checkpassword)
 
 cherrypy.tools.conditional_auth = Tool()
