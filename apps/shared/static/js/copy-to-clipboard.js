fdf33675ceab0c1f2c1272309781e19e8765281b
Bill Lovett <bill@ilovett.com>
2018-01-16 13:21:07 -0500

    [captures] Replace select-all link with copy-to-clipboard
    Switch copy-to-clipboard.js surrogate element from input to textarea
    to accommodate multiline values. Allow copy target to be specified via
    selector.

--- apps/shared/static/js/copy-to-clipboard.js
+++ apps/shared/static/js/copy-to-clipboard.js
@@ -1,22 +1,15 @@
 MEDLEY.copyToClipboard = (function () {
     'use strict';
 
-    function copy(e) {
-        e.preventDefault();
-        var copyTarget = jQuery(this).data('copytarget');
-        if (!copyTarget) {
-            return;
-        }
-
-
-        jQuery('#copy-to-clipboard-surrogate').val(copyTarget).select();
+    function copyValue(value, trigger) {
+        jQuery('#copy-to-clipboard-surrogate').val(value).select();
         document.execCommand('copy');
         jQuery('#copy-to-clipboard-surrogate').blur().val('');
 
-        var icon = jQuery('.icon-copy', this);
+        var icon = jQuery('.icon-copy', trigger);
         var messageHolder = jQuery('.message-holder');
         icon.addClass('copied');
-        messageHolder.text('Copied!');
+        messageHolder.text('Copied!').show();
 
         setTimeout(function () {
             icon.removeClass('copied');
@@ -27,11 +20,31 @@ MEDLEY.copyToClipboard = (function () {
         }, 1000);
     }
 
+    function findValue(e) {
+        e.preventDefault();
+        var trigger, selector, value;
+
+        trigger = jQuery(this);
+
+        value = jQuery(this).data('copytarget');
+
+        if (!value) {
+            selector = jQuery(this).data('copyselector');
+            value = jQuery(selector).text();
+        }
+
+        if (!value) {
+            return;
+        }
+
+        copyValue(value, trigger);
+    }
+
     return {
         init: function () {
-            var surrogate = jQuery('<form style="position:absolute;top:-999em;left:-999em"><input type="text" id="copy-to-clipboard-surrogate" value="" /></form>');
+            var surrogate = jQuery('<form style="position:absolute;top:-999em;left:-999em"><textarea id="copy-to-clipboard-surrogate"></textarea></form>');
             jQuery('body').append(surrogate);
-            jQuery('.copy-to-clipboard').on('click', copy);
+            jQuery('.copy-to-clipboard').on('click', findValue);
         }
     };
 
