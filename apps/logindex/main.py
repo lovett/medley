cae72c0baf21dfa143915842c1ac427bd2b59e8e
Bill Lovett <bill@ilovett.com>
2018-03-10 16:25:01 -0500

    [logindex] Linting fixes

--- apps/logindex/main.py
+++ apps/logindex/main.py
@@ -1,9 +1,17 @@
+"""
+Trigger indexing of log files for display by the visitors app
+"""
+
 import datetime
-import cherrypy
 import os.path
+import cherrypy
+
 
 class Controller:
-    """Index log files"""
+    """
+    The primary controller for the application, structured for
+    method-based dispatch
+    """
 
     name = "Log Index"
 
@@ -11,28 +19,39 @@ class Controller:
 
     user_facing = False
 
-    def parseLogDate(self, val):
+    @staticmethod
+    def parse_log_date(val):
+        """
+        Convert a datestamp in YYYY-mm-dd format to a datetime
+
+        If parsing fails, raise a 400 error.
+        """
+
         filename = os.path.splitext(val)[0]
         try:
             return datetime.datetime.strptime(filename, "%Y-%m-%d")
-        except:
+        except ValueError:
             raise cherrypy.HTTPError(
                 400,
                 "Unable to parse a date from {}".format(filename)
             )
 
     def POST(self, start, end=None):
-        start_date = self.parseLogDate(start)
+        """
+        Initiate log file parsing within a specified date range.
+        """
+
+        start_date = self.parse_log_date(start)
         end_date = start_date
 
         if end:
-            end_date = self.parseLogDate(end)
+            end_date = self.parse_log_date(end)
             if start_date > end_date:
                 raise cherrypy.HTTPError(400, "Invalid date range")
 
         index_date = start_date
         while index_date <= end_date:
-            cherrypy.engine.publish("logindex:enqueue",index_date)
+            cherrypy.engine.publish("logindex:enqueue", index_date)
             index_date += datetime.timedelta(days=1)
 
         cherrypy.engine.publish("logindex:parse")
--- apps/logindex/test_main.py
+++ apps/logindex/test_main.py
@@ -1,27 +1,38 @@
-from testing import assertions
-from testing import cptestcase
-from testing import helpers
-import apps.logindex.main
-import cherrypy
-import mock
+"""
+Test suite for the logindex app
+"""
+
 import unittest
 import datetime
+import mock
+from testing.assertions import ResponseAssertions
+from testing import helpers
+from testing.cptestcase import BaseCherryPyTestCase
+import apps.logindex.main
 
-class TestLater(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
+
+class TestLater(BaseCherryPyTestCase, ResponseAssertions):
+    """
+    Tests for the whois application controller
+    """
 
     @classmethod
     def setUpClass(cls):
+        """Start a faux cherrypy server"""
         helpers.start_server(apps.logindex.main.Controller)
 
     @classmethod
     def tearDownClass(cls):
+        """Shut down the faux server"""
         helpers.stop_server()
 
     def test_allow(self):
+        """Verify the controller's supported HTTP methods"""
         response = self.request("/", method="HEAD")
         self.assertAllowedMethods(response, ("POST",))
 
-    def test_invalidStart(self):
+    def test_invalid_start(self):
+        """An error should be thrown if the start date cannot be parsed"""
         response = self.request(
             "/",
             method="POST",
@@ -29,7 +40,8 @@ class TestLater(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
         )
         self.assertEqual(response.code, 400)
 
-    def test_invalidEnd(self):
+    def test_invalid_end(self):
+        """An error should be thrown if the end date cannot be parsed"""
         response = self.request(
             "/",
             method="POST",
@@ -39,41 +51,63 @@ class TestLater(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
         self.assertEqual(response.code, 400)
 
     @mock.patch("cherrypy.engine.publish")
-    def test_validStart(self, publishMock):
-        def side_effect(*args, **kwargs):
-            if args[0] == "registry:first_value" and args[1] == "logindex:root":
-                return ["/tmp"]
+    def test_valid_range(self, publish_mock):
+        """
+        The logindex plugin is called when a valid time range is given.
+        """
 
-        response = self.request(
+        self.request(
             "/",
             method="POST",
             start="2017-01-01.log",
             end="2017-01-03.log"
         )
 
-        calls = publishMock.call_args_list
+        calls = publish_mock.call_args_list
 
-        self.assertEqual(calls[-4], mock.call('logindex:enqueue', datetime.datetime(2017, 1, 1, 0, 0)))
-        self.assertEqual(calls[-3], mock.call('logindex:enqueue', datetime.datetime(2017, 1, 2, 0, 0)))
-        self.assertEqual(calls[-2], mock.call('logindex:enqueue', datetime.datetime(2017, 1, 3, 0, 0)))
-        self.assertEqual(calls[-1], mock.call('logindex:schedule_parse'))
+        self.assertEqual(
+            calls[-4],
+            mock.call('logindex:enqueue', datetime.datetime(2017, 1, 1, 0, 0))
+        )
+
+        self.assertEqual(
+            calls[-3],
+            mock.call('logindex:enqueue', datetime.datetime(2017, 1, 2, 0, 0))
+        )
+
+        self.assertEqual(
+            calls[-2],
+            mock.call('logindex:enqueue', datetime.datetime(2017, 1, 3, 0, 0))
+        )
+
+        self.assertEqual(
+            calls[-1],
+            mock.call('logindex:parse')
+        )
 
     @mock.patch("cherrypy.engine.publish")
-    def test_onlyStart(self, publishMock):
-        def side_effect(*args, **kwargs):
-            if args[0] == "registry:first_value" and args[1] == "logindex:root":
-                return ["/tmp"]
-
-        response = self.request(
+    def test_only_start(self, publish_mock):
+        """
+        A single-day time range can be specified by only specifying the
+        start date.
+        """
+        self.request(
             "/",
             method="POST",
             start="2017-01-01.log"
         )
 
-        calls = publishMock.call_args_list
+        calls = publish_mock.call_args_list
 
-        self.assertEqual(calls[-2], mock.call('logindex:enqueue', datetime.datetime(2017, 1, 1, 0, 0)))
-        self.assertEqual(calls[-1], mock.call('logindex:schedule_parse'))
+        self.assertEqual(
+            calls[-2],
+            mock.call('logindex:enqueue', datetime.datetime(2017, 1, 1, 0, 0))
+        )
+
+        self.assertEqual(
+            calls[-1], mock.call('logindex:parse')
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
