1ca30b276ac5bdf8ebaf5782e13bc9a22e060a98
Bill Lovett <bill@ilovett.com>
2018-03-31 19:31:35 -0400

    Miscellaneous linting cleanup

--- plugins/decorators.py
+++ plugins/decorators.py
@@ -2,17 +2,24 @@ import time
 import functools
 import cherrypy
 
+
 def log_runtime_in_applog(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         t0 = time.perf_counter()
         result = f(*args, **kwargs)
         t1 = time.perf_counter()
+        source = '{}.{}'.format(f.__module__, f.__name__)
+        key = 'runtime'
+        duration = t1 - t0
+
         cherrypy.engine.publish(
-            "applog:add",
-            "{}.{}".format(f.__module__, f.__name__),
-            "runtime",
-            t1 - t0
+            'applog:add',
+            source,
+            key,
+            duration
         )
+
         return result
+
     return wrapper
