71873c077671dab771427ad5e4ae18d62a131d0a
Bill Lovett <bill@ilovett.com>
2018-04-16 13:48:43 -0400

    [azure] Linting fixes

--- apps/azure/main.py
+++ apps/azure/main.py
@@ -1,7 +1,6 @@
 """Relay deployment notifications from Azure to notifier."""
 
 import cherrypy
-import tools.capture
 
 
 class Controller:
@@ -11,9 +10,12 @@ class Controller:
 
     user_facing = False
 
+    @staticmethod
     @cherrypy.tools.json_in()
     @cherrypy.tools.capture()
-    def POST(self):
+    def POST():
+        """Transform a deployment notification from Azure to a notification."""
+
         details = cherrypy.request.json
 
         if not details.get("siteName"):
@@ -25,18 +27,20 @@ class Controller:
             "title": "Deployment to {}".format(details["siteName"])
         }
 
-        azure_portal_url = cherrypy.engine.publish("registry:first_value", "azure:portal_url").pop()
+        azure_portal_url = cherrypy.engine.publish(
+            "registry:first_value",
+            "azure:portal_url"
+        ).pop()
 
         if azure_portal_url:
             notification["url"] = azure_portal_url.format(details["siteName"])
 
-        if details.get("status") == "success" and details.get("complete") == True:
+        if details.get("status") == "success" and details.get("complete"):
             notification["title"] += " is complete"
         elif details.get("status") == "failed":
             notification["title"] += " has failed"
         else:
             notification["title"] += " has uncertain status"
-
 
         cherrypy.engine.publish("notifier:send", notification)
 
--- apps/azure/test_main.py
+++ apps/azure/test_main.py
@@ -1,25 +1,32 @@
-from testing import assertions
-from testing import cptestcase
-from testing import helpers
-import unittest
-import apps.azure.main
-import mock
+"""Test suite for the azure app"""
 
-class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
+import unittest
+import mock
+from testing.assertions import ResponseAssertions
+from testing import helpers
+from testing.cptestcase import BaseCherryPyTestCase
+import apps.azure.main
+
+
+class TestAzure(BaseCherryPyTestCase, ResponseAssertions):
+    """Tests for the azure application controller."""
 
     @classmethod
     def setUpClass(cls):
+        """Start a faux cherrypy server"""
         helpers.start_server(apps.azure.main.Controller)
 
     @classmethod
     def tearDownClass(cls):
+        """Shut down the faux server"""
         helpers.stop_server()
 
     def test_allow(self):
+        """Verify the controller's supported HTTP methods"""
         response = self.request("/", method="HEAD")
         self.assertAllowedMethods(response, ("POST",))
 
-    def test_requiresSiteName(self):
+    def test_requires_site_name(self):
         """The request body must specify a site name"""
         response = self.request("/", method="POST", json_body={
             "status": "success"
@@ -27,14 +34,16 @@ class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
         self.assertEqual(response.code, 400)
 
     @mock.patch("cherrypy.engine.publish")
-    def test_sendsSuccessNotification(self, publishMock):
+    def test_sends_success_notification(self, publish_mock):
         """A success status triggers a success notification"""
 
-        def side_effect(*args, **kwargs):
-            if (args[0] == "registry:first_value"):
+        def side_effect(*args, **_):
+            """Side effects local function"""
+            if args[0] == "registry:first_value":
                 return ["http://example.com/{}"]
+            return mock.DEFAULT
 
-        publishMock.side_effect = side_effect
+        publish_mock.side_effect = side_effect
 
         response = self.request("/", method="POST", json_body={
             "siteName": "azuretest",
@@ -42,21 +51,30 @@ class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
             "complete": True
         })
 
-        notification = publishMock.call_args[0][1]
+        notification = publish_mock.call_args[0][1]
 
         self.assertEqual(response.code, 204)
-        self.assertEqual("Deployment to azuretest is complete", notification["title"])
-        self.assertEqual("http://example.com/azuretest", notification["url"])
+        self.assertEqual(
+            "Deployment to azuretest is complete",
+            notification["title"]
+        )
+
+        self.assertEqual(
+            "http://example.com/azuretest",
+            notification["url"]
+        )
 
     @mock.patch("cherrypy.engine.publish")
-    def test_sendsFailureNotification(self, publishMock):
+    def test_sends_failure_notification(self, publish_mock):
         """A failed status triggers a failure notification"""
 
-        def side_effect(*args, **kwargs):
-            if (args[0] == "registry:first_value"):
+        def side_effect(*args, **_):
+            """Side effects local function"""
+            if args[0] == "registry:first_value":
                 return ["http://example.com/{}"]
+            return mock.DEFAULT
 
-        publishMock.side_effect = side_effect
+        publish_mock.side_effect = side_effect
 
         response = self.request("/", method="POST", json_body={
             "siteName": "azuretest",
@@ -64,23 +82,32 @@ class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
             "complete": True
         })
 
-        notification = publishMock.call_args[0][1]
+        notification = publish_mock.call_args[0][1]
 
         self.assertEqual(response.code, 204)
-        self.assertEqual("Deployment to azuretest has failed", notification["title"])
-        self.assertEqual("http://example.com/azuretest", notification["url"])
+        self.assertEqual(
+            "Deployment to azuretest has failed",
+            notification["title"]
+        )
+
+        self.assertEqual(
+            "http://example.com/azuretest",
+            notification["url"]
+        )
 
     @mock.patch("cherrypy.engine.publish")
-    def test_sendsUnknownStatusNotification(self, publishMock):
+    def test_sends_unknown_status(self, publish_mock):
         """A status that is neither failed nor success triggers sends an
         unknown status notification
         """
 
-        def side_effect(*args, **kwargs):
-            if (args[0] == "registry:first_value"):
+        def side_effect(*args, **_):
+            """Side effects local function"""
+            if args[0] == "registry:first_value":
                 return ["http://example.com/{}"]
+            return mock.DEFAULT
 
-        publishMock.side_effect = side_effect
+        publish_mock.side_effect = side_effect
 
         response = self.request("/", method="POST", json_body={
             "siteName": "azuretest",
@@ -88,21 +115,30 @@ class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
             "complete": True
         })
 
-        notification = publishMock.call_args[0][1]
+        notification = publish_mock.call_args[0][1]
 
         self.assertEqual(response.code, 204)
-        self.assertEqual("Deployment to azuretest has uncertain status", notification["title"])
-        self.assertEqual("http://example.com/azuretest", notification["url"])
+        self.assertEqual(
+            "Deployment to azuretest has uncertain status",
+            notification["title"]
+        )
+
+        self.assertEqual(
+            "http://example.com/azuretest",
+            notification["url"]
+        )
 
     @mock.patch("cherrypy.engine.publish")
-    def test_skipsUrlIfPortalNotConfigured(self, publishMock):
+    def test_skips_url_if_no_portal(self, publish_mock):
         """A failed status triggers a failure notification"""
 
-        def side_effect(*args, **kwargs):
-            if (args[0] == "registry:first_value"):
+        def side_effect(*args, **_):
+            """Side effects local function"""
+            if args[0] == "registry:first_value":
                 return [None]
+            return mock.DEFAULT
 
-        publishMock.side_effect = side_effect
+        publish_mock.side_effect = side_effect
 
         response = self.request("/", method="POST", json_body={
             "siteName": "azuretest",
@@ -110,7 +146,7 @@ class TestAzure(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
             "complete": True
         })
 
-        notification = publishMock.call_args[0][1]
+        notification = publish_mock.call_args[0][1]
 
         self.assertEqual(response.code, 204)
         self.assertNotIn("url", notification)
