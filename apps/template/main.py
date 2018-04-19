cda110ec9f99a921c31a93288ab353439c1e81a3
Bill Lovett <bill@ilovett.com>
2018-04-18 20:18:01 -0400

    [template] Linting fixes

--- apps/template/main.py
+++ apps/template/main.py
@@ -1,22 +1,16 @@
 """One-line summary of the app goes here"""
+
 import cherrypy
+
 
 class Controller:
     """Dispatch application requests based on HTTP verb."""
 
     name = "Template"
 
-    cache_key = "template:cache"
-
     @cherrypy.tools.negotiable()
     def GET(self):
-
-        config = cherrypy.engine.publish(
-            "registry:search",
-            "template:*",
-        ).pop()
-
-        cached_value = cherrypy.engine.publish("cache:get", self.cache_key).pop()
+        """Lorem ipsum dolor sit"""
 
         return {
             "html": ("template.html", {
--- apps/template/test_main.py
+++ apps/template/test_main.py
@@ -1,46 +1,50 @@
-from testing import assertions
-from testing import cptestcase
-from testing import helpers
-import apps.template.main
-import cherrypy
-import datetime
-import mock
-import unittest
+"""
+Test suite for the template app
+"""
 
-class TestTemplate(cptestcase.BaseCherryPyTestCase, assertions.ResponseAssertions):
+import unittest
+import mock
+from testing.assertions import ResponseAssertions
+from testing import helpers
+from testing.cptestcase import BaseCherryPyTestCase
+import apps.template.main
+
+
+class TestTemplate(BaseCherryPyTestCase, ResponseAssertions):
     """Unit tests for the template app"""
 
     @classmethod
     def setUpClass(cls):
+        """Start a faux cherrypy server"""
         helpers.start_server(apps.template.main.Controller)
 
     @classmethod
     def tearDownClass(cls):
+        """Shut down the faux server"""
         helpers.stop_server()
 
-    def extract_template_vars(self, mock):
-        return mock.call_args[0][0]["html"][1]
-
     def test_allow(self):
+        """Verify the controller's supported HTTP methods"""
         response = self.request("/", method="HEAD")
         self.assertAllowedMethods(response, ("GET",))
 
-    @mock.patch("cherrypy.tools.negotiable._renderHtml")
     @mock.patch("cherrypy.engine.publish")
-    def test_template(self, publishMock, renderMock):
-        def side_effect(*args, **kwargs):
+    def test_template(self, publish_mock):
+        """Lorem ipsum dolor sit"""
+
+        def side_effect(*args, **_):
+            """Side effects local function"""
             if args[0] == "registry:search":
                 return [None]
             if args[0] == "cache:get":
                 return [None]
+            return mock.DEFAULT
 
-        publishMock.side_effect = side_effect
+        publish_mock.side_effect = side_effect
 
-        response = self.request("/")
+        self.request("/")
 
-        template_vars = self.extract_template_vars(renderMock)
-
-        self.assertTrue(True)
+        self.assertTrue(1 > 0)
 
 
 if __name__ == "__main__":
