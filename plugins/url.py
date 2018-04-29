16330c8de97af6bc33fca09483f0d4dcf392f45c
Bill Lovett <bill@ilovett.com>
2018-04-28 20:28:16 -0400

    [plugins] Lint url plugin

--- plugins/url.py
+++ plugins/url.py
@@ -1,19 +1,27 @@
+"""Work out an app URL from a controller instance."""
+
+from urllib.parse import urlencode
 import cherrypy
 from cherrypy.process import plugins
-from urllib.parse import urlencode
 
 
 class Plugin(plugins.SimplePlugin):
+    """A CherryPy plugin for building app-specific URLs."""
+
     def __init__(self, bus):
         plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
+        """Define the CherryPy messages to listen for.
+
+        This plugin owns the url prefix.
+        """
         self.bus.subscribe("url:for_controller", self.url_for_controller)
 
-    def stop(self):
-        pass
+    @staticmethod
+    def url_for_controller(controller, path=None, query=()):
+        """Create a URL for an application given its controller."""
 
-    def url_for_controller(self, cls, path=None, query={}):
         host = cherrypy.request.headers.get("Host", "")
 
         if cherrypy.request.headers.get("X-Https", "") == "On":
@@ -23,8 +31,8 @@ class Plugin(plugins.SimplePlugin):
 
         url = next(
             ("{}://{}{}".format(proto, host, key)
-             for key in cherrypy.tree.apps.keys()
-             if isinstance(cherrypy.tree.apps[key].root, type(cls))),
+             for key in cherrypy.tree.apps
+             if isinstance(cherrypy.tree.apps[key].root, type(controller))),
             None
         )
 
