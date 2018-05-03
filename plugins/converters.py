e08ea88b945c9ef4a37e71aa842a3d0935a5acb2
Bill Lovett <bill@ilovett.com>
2018-05-02 22:39:02 -0400

    [plugins] Lint converters plugin

--- plugins/converters.py
+++ plugins/converters.py
@@ -1,19 +1,30 @@
-import cherrypy
+"""Custom datatype conversions for use with Python's DB-API interface."""
+
 import datetime
-import msgpack
 import pickle
-import pytz
 import re
-import sqlite3
 import urllib
+import sqlite3
 from tzlocal import get_localzone
+import cherrypy
+import msgpack
+import pytz
 import pendulum
 
+
 class Plugin(cherrypy.process.plugins.SimplePlugin):
+    """A CherryPy plugin that registers sqlite3 datatype converters.
+
+    Unlike other plugins, this one doesn't subscribe to any events and
+    isn't meant for pubsub interaction.
+
+    """
+
     def __init__(self, bus):
         cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
+        """Register converters."""
         sqlite3.register_converter("datetime", self.datetime)
         sqlite3.register_converter("binary", self.binary)
         sqlite3.register_converter("calldate_to_utc", self.calldate_to_utc)
@@ -21,45 +32,57 @@ class Plugin(cherrypy.process.plugins.SimplePlugin):
         sqlite3.register_converter("clid", self.callerid)
         sqlite3.register_converter("querystring", self.querystring)
 
-    def stop(self):
-        pass
+    @staticmethod
+    def datetime(value):
+        """Convert a datetime string to a datetime instance."""
 
-    def datetime(self, s):
-        s = s.decode("utf-8")
+        value = value.decode("utf-8")
         try:
-            d = datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
-            return pytz.utc.localize(d)
+            date = datetime.datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+            return pytz.utc.localize(date)
         except ValueError:
-            last_colon_index = s.rindex(":")
-            date = s[:last_colon_index] + s[last_colon_index + 1:]
+            last_colon_index = value.rindex(":")
+            date = value[:last_colon_index] + value[last_colon_index + 1:]
             return datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S%z")
 
-    def local_timezone(self):
-        tz = cherrypy.engine.publish(
+    @staticmethod
+    def local_timezone():
+        """Determine the timezone of the application.
+
+        The registry is checked first so that the application timezone
+        can be independent of the server's timezone. But the server's
+        timezone also acts as a fallback.
+
+        """
+
+        timezone = cherrypy.engine.publish(
             "registry:first_value",
             "config:timezone",
             memorize=True
         ).pop()
 
-        if not tz:
-            tz = get_localzone()
+        if not timezone:
+            timezone = get_localzone()
 
-        return tz
+        return timezone
 
-    def calldate_to_utc(self, s):
+    def calldate_to_utc(self, value):
         """Convert a local datetime string to a UTC Pendulum instance."""
 
         local_tz = self.local_timezone()
 
         return pendulum.from_format(
-            s.decode("utf-8"),
+            value.decode("utf-8"),
             "YYYY-MM-DD HH:mm:ss",
             tz=local_tz,
             formatter='alternative'
         ).in_timezone('utc')
 
-    def duration(self, s):
-        seconds = int(s)
+    @staticmethod
+    def duration(value):
+        """Convert a number of seconds into a human-readable string."""
+
+        seconds = int(value)
         hours_label = "hour"
         minutes_label = "minute"
         seconds_label = "second"
@@ -88,19 +111,37 @@ class Plugin(cherrypy.process.plugins.SimplePlugin):
         if seconds > 0:
             result.append("{} {}".format(seconds, seconds_label))
 
-        if len(result) == 0:
+        if not result:
             result.append("0 {}".format(seconds_label))
 
         return ", ".join(result)
 
-    def callerid(self, s):
-        return re.sub(r'"(.*?)".*', r"\1", s.decode("utf-8"))
+    @staticmethod
+    def callerid(value):
+        """De-quote a caller ID string from an Asterisk CDR database."""
 
-    def binary(self, blob):
+        return re.sub(r'"(.*?)".*', r"\1", value.decode("utf-8"))
+
+    @staticmethod
+    def binary(blob):
+        """Unpack a binary value stored in a blob field.
+
+        MessagePack is the only serialization format used by the
+        application, but fall back to pickle since that was the format
+        used before MessagePack was implemented.
+
+        """
+
         try:
             return msgpack.unpackb(blob, encoding='utf-8')
         except msgpack.exceptions.ExtraData:
             return pickle.loads(blob)
 
-    def querystring(self, s):
-        return urllib.parse.parse_qs(s.decode("utf-8"), keep_blank_values=True)
+    @staticmethod
+    def querystring(value):
+        """Parse a URL querystring into a dict."""
+
+        return urllib.parse.parse_qs(
+            value.decode("utf-8"),
+            keep_blank_values=True
+        )
