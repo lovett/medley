53953c0f2a39281f60d0587ec66f378cff509a62
Bill Lovett <bill@ilovett.com>
2018-05-03 22:06:24 -0400

    [plugins] Lint cache plugin

--- plugins/cache.py
+++ plugins/cache.py
@@ -1,9 +1,13 @@
-import cherrypy
+"""Store arbitrary values in an SQLite database."""
+
 import time
+import cherrypy
 import msgpack
 from . import mixins
 
+
 class Plugin(cherrypy.process.plugins.SimplePlugin, mixins.Sqlite):
+    """A CherryPy plugin for caching arbitrary values to disk."""
 
     def __init__(self, bus):
         cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
@@ -17,19 +21,26 @@ class Plugin(cherrypy.process.plugins.SimplePlugin, mixins.Sqlite):
             )""")
 
     def start(self):
+        """Define the CherryPy messages to listen for.
+
+        This plugin owns the cdr prefix.
+        """
+
         self.bus.subscribe("cache:get", self.get)
         self.bus.subscribe("cache:set", self.set)
         self.bus.subscribe("cache:clear", self.clear)
 
-    def stop(self):
-        pass
-
     def get(self, key):
-        """Retrieve a value from the cache by its key"""
+        """Retrieve a value from the store by its key."""
 
         self.prune(key)
 
-        row = self._selectOne("SELECT value as 'value [binary]', created as 'created [datetime]' FROM cache WHERE key=?", (key,))
+        row = self._selectOne(
+            """SELECT value as 'value [binary]', created as 'created [datetime]'
+            FROM cache
+            WHERE key=?""",
+            (key,)
+        )
 
         if "value" in row.keys():
             cherrypy.engine.publish("applog:add", "cache", "hit", key)
@@ -39,20 +50,39 @@ class Plugin(cherrypy.process.plugins.SimplePlugin, mixins.Sqlite):
         return False
 
     def set(self, key, value, lifespan_seconds=3600):
+        """Add a value to the store."""
+
         expires = time.time() + int(lifespan_seconds)
         packed_value = msgpack.packb(value, use_bin_type=True)
         self._insert(
-            "INSERT OR REPLACE INTO cache (key, value, expires) VALUES (?, ?, ?)",
+            """INSERT OR REPLACE INTO cache
+            (key, value, expires)
+            VALUES (?, ?, ?)""",
             [(key, packed_value, expires)]
         )
         return True
 
     def clear(self, key):
+        """Remove a value from the store by its key."""
         deletions = self._delete("DELETE FROM cache WHERE key=?", (key,))
-        cherrypy.engine.publish("applog:add", "cache", "clear:{}".format(key), deletions)
+        cherrypy.engine.publish(
+            "applog:add",
+            "cache",
+            "clear:{}".format(key),
+            deletions
+        )
         return deletions
 
     def prune(self, key):
-        """Delete expired cache entries by key"""
-        deletions = self._delete("DELETE FROM cache WHERE key=? AND expires < ?", (key, time.time()))
-        cherrypy.engine.publish("applog:add", "cache", "prune:{}".format(key), deletions)
+        """Delete expired cache entries by key."""
+
+        deletions = self._delete(
+            "DELETE FROM cache WHERE key=? AND expires < ?",
+            (key, time.time())
+        )
+        cherrypy.engine.publish(
+            "applog:add",
+            "cache",
+            "prune:{}".format(key),
+            deletions
+        )
