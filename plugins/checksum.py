817a38504c2a9ab4487f05a6bd1afe98c4cf3043
Bill Lovett <bill@ilovett.com>
2018-05-17 23:55:37 -0400

    [plugins] Lint checksum plugin

--- plugins/checksum.py
+++ plugins/checksum.py
@@ -1,37 +1,46 @@
-import cherrypy
-import time
-import os
+"""Calculate checksums for files and strings."""
+
 import zlib
+import cherrypy
+
 
 class Plugin(cherrypy.process.plugins.SimplePlugin):
+    """A CherryPy plugin for calculating checksums."""
 
     def __init__(self, bus):
         cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
-        self.bus.subscribe("checksum:file", self.byPath)
-        self.bus.subscribe("checksum:string", self.byString)
+        """Define the CherryPy messages to listen for.
 
-    def stop(self):
-        pass
+        This plugin owns the checksum prefix.
+        """
+        self.bus.subscribe("checksum:file", self.adler32_path)
+        self.bus.subscribe("checksum:string", self.adler32_string)
 
-    def byString(self, val):
+    @staticmethod
+    def adler32_string(val):
+        """Calculate the adler32 checksum of a string."""
+
         result = zlib.adler32(bytes(val, "utf-8"), 0)
         result_hash = '%x' % (result & 0xFFFFFFFF)
         return result_hash
 
-    def byPath(self, path):
+    @staticmethod
+    def adler32_path(path):
+        """Calculate the adler32 checksum of a file."""
+
         memorized_hash = cherrypy.engine.publish("memorize:get", path).pop()
-        if memorized_hash[0] == True:
+        if memorized_hash[0]:
             return memorized_hash[1]
 
         result = 0
-        with open(path, "rb") as f:
-            result = zlib.adler32(f.read(), result)
+        with open(path, "rb") as file_handle:
+            result = zlib.adler32(file_handle.read(), result)
 
         result_hash = '%x' % (result & 0xFFFFFFFF)
 
-        if cherrypy.config.get("memorize_checksums") == True:
+        if cherrypy.config.get("memorize_checksums"):
             cherrypy.engine.publish(
                 "memorize:set",
                 path, result_hash
