8a318dd2fec5cb305cbb1f2b525915519f6ccaf7
Bill Lovett <bill@ilovett.com>
2018-05-31 12:08:19 -0400

    [plugins] Lint jinja plugin

--- plugins/jinja.py
+++ plugins/jinja.py
@@ -1,20 +1,24 @@
-# taken from https://bitbucket.org/Lawouach/cherrypy-recipes/src/d140e6da973a/web/templating/jinja2_templatin
-import cherrypy
-import jinja2
+"""Render a Jinja2 template.
+
+Based on sample code from https://bitbucket.org/Lawouach/cherrypy-recipes.
+"""
+
 import http.client
+import os
 import os.path
 import urllib
-import os
+from urllib.parse import quote
 import json
 import re
-import pendulum
-from urllib.parse import quote
 from cherrypy.process import plugins
-from string import Template
+import cherrypy
+import jinja2
+import pendulum
 
 
+# pylint: disable=too-many-public-methods
 class Plugin(plugins.SimplePlugin):
-    """A WSPBus plugin that manages Jinja2 templates"""
+    """A CherryPy plugin for rendering Jinja2 templates."""
 
     def __init__(self, bus):
         server_root = cherrypy.config.get("server_root")
@@ -40,15 +44,14 @@ class Plugin(plugins.SimplePlugin):
         apps = filter(lambda x: not x.endswith("__"), apps)
         paths.extend(apps)
 
-
         loader = jinja2.FileSystemLoader(paths)
 
         cache = jinja2.FileSystemBytecodeCache(cache_dir, '%s.cache')
 
         self.env = jinja2.Environment(
-            loader = loader,
-            auto_reload = cherrypy.config.get("engine.autoreload.on"),
-            bytecode_cache = cache
+            loader=loader,
+            auto_reload=cherrypy.config.get("engine.autoreload.on"),
+            bytecode_cache=cache
         )
 
         self.env.filters["dateformat"] = self.dateformat_filter
@@ -76,22 +79,16 @@ class Plugin(plugins.SimplePlugin):
         plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
-        """Called when the engine starts"""
-        self.bus.log('Setting up Jinja2')
+        """Define the CherryPy messages to listen for.
+
+        This plugin owns the jinja prefix
+        """
+
         self.bus.subscribe("lookup-template", self.get_template)
 
-    def stop(self):
-        """Called when the engine stops"""
-        self.bus.log('Freeing Jinja2 resources')
-        self.bus.unsubscribe("lookup-template", self.get_template)
-        self.env = None
-
     def get_template(self, name):
-        """Returns Jinja2's template by name
+        """Retrieve a Jinja2 template by name."""
 
-        Usage:
-        >>> template = cherrypy.engine.publish('lookup-template', 'index.html').pop()
-        """
         return self.env.get_template(name)
 
     def localtime_filter(self, value, timezone=None):
@@ -101,22 +98,27 @@ class Plugin(plugins.SimplePlugin):
             return ""
 
         if not timezone:
-            tz = self.local_timezone()
+            zone = self.local_timezone()
 
         if isinstance(value, (int, float)):
             value = pendulum.from_timestamp(value)
         else:
             value = pendulum.instance(value)
 
-        return value.in_timezone(tz)
+        return value.in_timezone(zone)
 
-    def dateformat_filter(self, value, format_string):
+    @staticmethod
+    def dateformat_filter(value, format_string):
         """Format a datetime instance to a string."""
 
         return value.format(format_string)
 
-    def unindent_filter(self, string):
-        """Remove leading whitespace from a multiline string without losing indentation"""
+    @staticmethod
+    def unindent_filter(string):
+        """Remove leading whitespace from a multiline string without losing
+        indentation
+
+        """
         if not string:
             return ""
 
@@ -126,22 +128,32 @@ class Plugin(plugins.SimplePlugin):
         unindented = [line.replace(" ", "", min(indents)) for line in lines]
         return "\n".join(unindented)
 
-    def status_message_filter(self, code):
+    @staticmethod
+    def status_message_filter(code):
         """Returns the standard status code message for the given integer"""
         return http.client.responses.get(int(code), "Unknown")
 
-    def nl2br_filter(self, s):
+    @staticmethod
+    def nl2br_filter(value):
         """Replace newlines with <br/> tags"""
-        return s.replace("\n", "<br/>")
+        return value.replace("\n", "<br/>")
 
-    def pluralize_filter(self, count, singular, plural=None, suffix=''):
+    @staticmethod
+    def pluralize_filter(count, singular, plural=None, suffix=''):
+        """Label a value with the singular or plural form of a word."""
+
         if not plural:
             plural = singular + "s"
 
         value = singular if count == 1 else plural
         return "{} {} {}".format(count, value, suffix)
 
-    def anonymize_filter(self, url):
+    @staticmethod
+    def anonymize_filter(url):
+        """Prepend a URL with the URL of an anonymizer to increase referrer
+        privacy.
+
+        """
         anonymizer = cherrypy.engine.publish(
             "registry:first_value",
             "config:url_anonymizer",
@@ -156,40 +168,58 @@ class Plugin(plugins.SimplePlugin):
 
         return anonymizer + quote(url)
 
-    def urlencode_filter(self, value):
+    @staticmethod
+    def urlencode_filter(value):
+        """Apply URL encoding to a value."""
+
         return urllib.parse.quote(value)
 
-    def local_timezone(self):
-        tz = cherrypy.engine.publish(
+    @staticmethod
+    def local_timezone():
+        """Determine the timezone to be used for references to local time."""
+
+        zone = cherrypy.engine.publish(
             "registry:first_value",
             "config:timezone",
             memorize=True
         ).pop()
 
-        if not tz:
-            tz = pendulum.now().timezone.name
+        if not zone:
+            zone = pendulum.now().timezone.name
 
-        return tz
+        return zone
 
     def ago_filter(self, unix_timestamp):
-        dt = pendulum.from_timestamp(unix_timestamp)
+        """Calculate a human-readable time delta between a timestamp and
+        now.
 
-        tz = self.local_timezone()
+        """
 
-        return dt.in_timezone(tz).diff_for_humans()
+        date = pendulum.from_timestamp(unix_timestamp)
 
-    def yearmonth_filter(self, value):
+        zone = self.local_timezone()
+
+        return date.in_timezone(zone).diff_for_humans()
+
+    @staticmethod
+    def yearmonth_filter(value):
+        """Format a datetime to a year-month string."""
         return value.strftime("%Y-%m")
 
-    def json_filter(self, value):
+    @staticmethod
+    def json_filter(value):
+        """Pretty-print a JSON value."""
         return json.dumps(value, sort_keys=True, indent=2)
 
-    def websearch_filter(self, value, engine, url_only=False, target="_blank"):
-        if engine is "google":
+    @staticmethod
+    def websearch_filter(value, engine, url_only=False, target="_blank"):
+        """Construct an offsite search URL for a term."""
+
+        if engine == "google":
             url = "https://www.google.com#q={}"
 
-        if engine is "bing":
-            url= "https://www.bing.com/search?q={}"
+        if engine == "bing":
+            url = "https://www.bing.com/search?q={}"
 
         if not url:
             raise jinja2.TemplateError("Unrecognized search engine")
@@ -199,15 +229,17 @@ class Plugin(plugins.SimplePlugin):
         if url_only:
             return url
 
-        return """<a href="{}" target="{}" rel="noopener noreferer">{}</a>""".format(
+        template = '<a href="{}" target="{}" rel="noopener noreferer">{}</a>'
+        return template.format(
             url, target, engine.capitalize()
         )
 
-    def phonenumber_filter(self, value, as_link=False):
+    @staticmethod
+    def phonenumber_filter(value, as_link=False):
         """Format a US phone number as a human-readable string"""
 
         formats = {
-             7: lambda x: "{}-{}".format(x[:3], x[3:]),
+            7: lambda x: "{}-{}".format(x[:3], x[3:]),
             10: lambda x: "({}) {}-{}".format(x[:3], x[3:6], x[6:]),
             11: lambda x: "({}) {}-{}".format(x[1:4], x[4:7], x[7:]),
         }
@@ -215,17 +247,17 @@ class Plugin(plugins.SimplePlugin):
         formatter = formats.get(len(value))
 
         if formatter:
-            formattedValue = formatter(value)
+            formatted_value = formatter(value)
         elif "stdexten-" in value:
-            formattedValue = value.replace("stdexten-", "")
+            formatted_value = value.replace("stdexten-", "")
         else:
-            formattedValue = value
+            formatted_value = value
 
-        if as_link and formattedValue != value:
-            template = Template("""<a href="/phone?number=$plainNumber" rel="noreferrer">$formattedNumber</a>""")
-            return template.substitute(plainNumber=value, formattedNumber=formattedValue)
+        if as_link and formatted_value != value:
+            template = """<a href="/phone?number={}" rel="noreferrer">{}</a>"""
+            return template.format(value, formatted_value)
 
-        return formattedValue
+        return formatted_value
 
     def snorql_filter(self, value):
         """Build a URL to dbpedia.org/snorql with the specified query"""
@@ -236,14 +268,27 @@ class Plugin(plugins.SimplePlugin):
 
         return "http://dbpedia.org/snorql?query={}".format(encoded_query)
 
-    def percentage_filter(self, value):
+    @staticmethod
+    def percentage_filter(value):
+        """Display a value as a percentage."""
+
         if value < 1:
             return str(round(value * 100)) + "%"
 
         return str(value) + "%"
 
+    @staticmethod
     @jinja2.contextfilter
-    def cache_bust_filter(self, context, url):
+    def cache_bust_filter(_, url):
+        """Calculate a cache-aware URL to a static asset.
+
+        A cache-aware URL is one that contains a unique identifier in
+        its querystring that changes whenever the file's content
+        changes. This allows the client to cache the content
+        aggressively but still see new versions.
+
+        """
+
         abs_path = cherrypy.config.get("app_root") + url
 
         try:
@@ -252,8 +297,14 @@ class Plugin(plugins.SimplePlugin):
             checksum = ""
         return "{}?{}".format(url, checksum)
 
+    @staticmethod
     @jinja2.contextfilter
-    def escapejs_filter(self, context, val):
+    def escapejs_filter(_, val):
+        """Escape special characters in JavaScript when rendered inline with
+        markup.
+
+        """
+
         return str(val).translate({
             ord('\\'): '\\u005C',
             ord('\''): '\\u0027',
@@ -269,31 +320,42 @@ class Plugin(plugins.SimplePlugin):
             ord('\u2029'): '\\u2029'
         })
 
-    def hostname_truncate_filter(self, val, length=4):
-        segments = val.split(".")[::-1]
-        slice = segments[:length]
-        return ".".join(slice[::-1])
+    @staticmethod
+    def hostname_truncate_filter(val, length=4):
+        """Slice a hostname by segment, avoiding awkward cutoffs."""
 
-    def logline_with_links_filter(self, record):
+        segments = val.split(".")[::-1]
+        segment_slice = segments[:length]
+        return ".".join(segment_slice[::-1])
+
+    @staticmethod
+    def logline_with_links_filter(record):
+        """Add hyperlinks to a log entry."""
+
         result = record["logline"]
 
         # ip
         link = """<a href="/visitors?query=ip+{0}"
         title="Search for visits from this address"
         rel="noreferrer">{0}</a>""".format(record["ip"])
+
         result = result.replace(record["ip"], link)
 
         return result
 
-    def slug_filter(self, value):
+    @staticmethod
+    def slug_filter(value):
+        """Reduce a string to a URL-friendly, alphanumeric form."""
+
         slug = value.lower()
 
         return re.sub(r"\s+", "-", slug)
 
-    def sane_callerid_filter(self, value, default="unknown caller"):
+    @staticmethod
+    def sane_callerid_filter(value, default="unknown caller"):
         """Prevent Google Voice callerid strings from being displayed.
 
-        Example: +12223334444@voice.google.com/srvenc-randomstring/otherrandomstring/
+        Example: +12223334444@voice.google.com/srvenc-abc123/def456/
 
         """
 
