b15fed5ae5ecd2eac9aecc701241e075402a2c7b
Bill Lovett <bill@ilovett.com>
2018-06-06 18:32:01 -0400

    [plugins] Lint markup plugin

--- plugins/markup.py
+++ plugins/markup.py
@@ -1,22 +1,27 @@
-import cherrypy
-from html.parser import HTMLParser
+"""Tools for working with text containing HTML markup."""
+
 from html.entities import name2codepoint
+from html.parser import HTMLParser
+import cherrypy
+
 
 class Plugin(cherrypy.process.plugins.SimplePlugin):
+    """A CherryPy plugin for text manipulation involving markup."""
 
     def __init__(self, bus):
         cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
 
     def start(self):
-        self.bus.subscribe("markup:reduce_title", self.reduceTitle)
-        self.bus.subscribe("markup:html_title", self.getTitle)
-        self.bus.subscribe("markup:plaintext", self.plainText)
+        """Define the CherryPy messages to listen for.
 
-    def stop(self):
-        pass
+        This plugin owns the markup prefix.
+        """
+        self.bus.subscribe("markup:reduce_title", self.reduce_title)
+        self.bus.subscribe("markup:plaintext", self.plain_text)
 
-    def reduceTitle(self, title):
-        """Remove site identifiers and noise from the title of an HTML document"""
+    def reduce_title(self, title):
+        """Remove site identifiers from the title of an HTML document."""
+
         title = title or ""
         reduced_title = title
         for char in "»|·—:-":
@@ -28,84 +33,82 @@ class Plugin(cherrypy.process.plugins.SimplePlugin):
 
         if reduced_title == title:
             return title
-        else:
-            return self.reduceTitle(reduced_title)
 
-    def getTitle(self, html, with_reduce=True):
-        """Extract the contents of the title tag from an HTML string"""
-        parser = HtmlTitleParser()
-        title = parser.parse(html)
-        if with_reduce:
-            title = self.reduceTitle(title)
-        return title
+        return self.reduce_title(reduced_title)
 
-    def htmlToText(self, html):
-        """Reduce an HTML document to the text nodes of the body tag"""
+    @staticmethod
+    def plain_text(html):
+        """Remove markup and entities from a string"""
         parser = HtmlTextParser()
         return parser.parse(html)
 
-    def plainText(self, html):
-        """Remove markup and entities from a string"""
-        parser = TextParser()
-        return parser.parse(html)
 
 class HtmlTitleParser(HTMLParser):
+    """Extract the title tag from an HTML document."""
+
     in_title_tag = False
     result = None
 
     def parse(self, markup):
+        """Parse an HTML string."""
         self.feed(markup)
         return self.result
 
     def handle_starttag(self, tag, attrs):
+        """Locate the title opening tag."""
         if not self.result and tag == "title":
             self.in_title_tag = True
 
     def handle_endtag(self, tag):
+        """Locate the title close tag."""
         if not self.result and tag == "title":
             self.in_title_tag = False
 
     def handle_data(self, data):
+        """Capture the text node of the title tag."""
+
         if not self.result and self.in_title_tag:
             self.result = data.strip()
 
+    def error(self, message):
+        cherrypy.engine.publish(
+            "applog:add",
+            "markup",
+            "HtmlTitleParser",
+            message
+        )
+
 
 class HtmlTextParser(HTMLParser):
+    """Convert an HTML document to plain text."""
+
     tag = None
     result = []
     blacklist = ["script", "style"]
 
     def parse(self, markup):
+        """Parse an HTML string."""
         self.result = []
         self.feed(markup)
         return " ".join(self.result)
 
     def handle_starttag(self, tag, attrs):
+        """Track the current tag."""
         self.tag = tag
 
     def handle_data(self, data):
+        """Capture the text node for non-blacklisted tags."""
+
         if self.tag not in self.blacklist:
             self.result.append(data.strip())
 
-class TextParser(HTMLParser):
-
-    def parse(self, text):
-        self.result = []
-        self.feed(text)
-        return " ".join(self.result)
-
-    def capture(self, s):
-        self.result.append(s.strip())
-
-    def handle_data(self, s):
-        self.capture(s)
-
     def handle_entityref(self, name):
-        self.capture(chr(name2codepoint[name]))
+        self.result.append(chr(name2codepoint[name]))
 
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        self.capture(c)
+    def error(self, message):
+        cherrypy.engine.publish(
+            "applog:add",
+            "markup",
+            "HtmlTextParser",
+            message
+        )
