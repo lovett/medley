caa65d578d17ae33b74f37e038e6236b14ce5003
Bill Lovett <bill@ilovett.com>
2018-07-14 00:26:51 -0400

    Restrict access to the maintenance app to localhost
    Treat the maintenance app as an internal resource and prevent it from
    being accessed outside of localhost, but also let this be overridden
    in the application config just in case.

--- tools/conditional_auth.py
+++ tools/conditional_auth.py
@@ -1,11 +1,13 @@
 import cherrypy
 
+
 class Tool(cherrypy.Tool):
-    """Prompt for basic authentication unless the request originates from a
-    whitelisted IP
+    """Prompt for basic authentication unless the request originates from
+    a whitelisted IP
 
     Whitelisted IPs should be specified in the global application
     config under the key tools.conditional_auth.whitelist
+
     """
 
     def __init__(self):
@@ -19,24 +21,34 @@ class Tool(cherrypy.Tool):
     def _checkpassword(self, realm, user, password):
         users = cherrypy.config.get("users")
         p = users.get(user)
-        return p and p == password or False
+        if not p:
+            return False
+
+        return p == password
 
     def _auth(self, whitelist=None):
+        """Decide whether to prompt for HTTP basic authentication."""
 
-        ips = whitelist.split()
-        ips.append("127.0.0")
+        # The maintenance app should only be accessed from localhost,
+        # unless the global application config says otherwise.
+        only_local = False
+        if cherrypy.request.script_name == "/maintenance":
+            only_local = cherrypy.config.get("local_maintenance", True)
 
-        address = None
-        for header in ("X-Real-Ip", "Remote-Addr"):
-            try:
-                address = cherrypy.request.headers[header]
-                break
-            except KeyError:
-                pass
+        ips = ["127.0.0"]
+        if not only_local:
+            ips.extend(whitelist.split())
 
-        if any(address.startswith(ip) for ip in ips):
-            return
+        address = cherrypy.request.headers.get("X-Real-IP")
 
-        cherrypy.tools.auth_basic.callable(realm="medley", checkpassword=self._checkpassword)
+        if not address:
+            address = cherrypy.request.headers.get("Remote-Addr")
+
+        if not any(address.startswith(ip) for ip in ips):
+            cherrypy.tools.auth_basic.callable(
+                realm="medley",
+                checkpassword=self._checkpassword
+            )
+
 
 cherrypy.tools.conditional_auth = Tool()
