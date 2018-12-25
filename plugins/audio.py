f9ffb7e644e29a12689e70f562dc5b579903fd61
Bill Lovett <bill@ilovett.com>
2018-12-24 21:28:09 -0500

    Make simpleaudio an optional dependency
    The audio plugin now handles the absence of the simpleaudio package
    gracefully. The package is moved to an additional requirements file so
    that it doesn't become a roadblock to getting the server up and running.

--- plugins/audio.py
+++ plugins/audio.py
@@ -1,11 +1,26 @@
 """Play audio files
 
-see https://github.com/hamiltron/py-simple-audio
+Playback is handled by the simpleaudio package. Getting it to install
+can be a challenge since headers for both Python and Alsa are
+needed. Dealing with this just to get the medley server to run doesn't
+make sense--audio playback is not a critical function, just nice
+to have.
+
+When simpleaudio is not present, the plugin will log an error message
+if asked to play a file but otherwise behave normally.
+
+See  https://github.com/hamiltron/py-simple-audio
+
 """
 
 import cherrypy
 from cherrypy.process import plugins
-import simpleaudio as sa
+
+# Failure to import simpleaudio is allowed.
+try:
+    import simpleaudio as SIMPLE_AUDIO  # pylint: disable=import-error
+except ModuleNotFoundError:
+    SIMPLE_AUDIO = None
 
 
 class Plugin(plugins.SimplePlugin):
@@ -19,11 +34,22 @@ class Plugin(plugins.SimplePlugin):
 
         This plugin owns the audio prefix.
         """
+        if not SIMPLE_AUDIO:
+            cherrypy.log("Audio playback is disabled")
+
         self.bus.subscribe('audio:wav:play', self.play)
 
     @staticmethod
     def play(path):
         """Play a file. So far only WAVE is supported."""
+
+        if not SIMPLE_AUDIO:
+            message = "Ignoring request to play {}".format(
+                path
+            )
+
+            cherrypy.log(message)
+            return
 
         cherrypy.engine.publish(
             "applog:add",
@@ -32,6 +58,6 @@ class Plugin(plugins.SimplePlugin):
             "playing {}".format(path)
         )
 
-        wave_obj = sa.WaveObject.from_wave_file(path)
+        wave_obj = SIMPLE_AUDIO.WaveObject.from_wave_file(path)
         play_obj = wave_obj.play()
         play_obj.wait_done()
