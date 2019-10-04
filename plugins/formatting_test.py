8e8d8b2b003d6b222db2bff43e732e96281be162
Bill Lovett <bill@ilovett.com>
2019-10-04 09:59:50 -0400

    Salvage old tests related to dbpedia abstract formatting

--- plugins/formatting_test.py
+++ plugins/formatting_test.py
@@ -30,8 +30,31 @@ class TestFormatting(unittest.TestCase):
 
     def test_phone_sanitize_empty(self):
         """An empty string is returned untouched"""
+
         initial = ""
         final = self.plugin.phone_sanitize(initial)
+        self.assertEqual(final, "")
+
+    def test_dbpedia_truncation(self):
+        """A comment with two sentences is reduced to the first two"""
+
+        initial = "First. Second. Third. Fourth. Fifth."
+        final = self.plugin.dbpedia_abstract(initial)
+        self.assertEqual(final, "First. Second.")
+
+    def test_dbpedia_punctuation(self):
+        """The abbreviated comment has correct punctuation"""
+
+        initial = "Punctuation is missing"
+        final = self.plugin.dbpedia_abstract(initial)
+        self.assertEqual(final, initial + ".")
+
+    def test_dbpedia_noise(self):
+        """Noise is removed from the abbreviated comment"""
+
+        initial = """The map to the right is now clickable;
+        click on an area code to go to the map for that code."""
+        final = self.plugin.dbpedia_abstract(initial)
         self.assertEqual(final, "")
 
 
--- plugins/geography_test.py
+++ plugins/geography_test.py
@@ -19,6 +19,49 @@ class TestGeography(unittest.TestCase):
         self.plugin = plugins.geography.Plugin(cherrypy.engine)
 
     @mock.patch("cherrypy.engine.publish")
+    def test_area_code_valid(self, publish_mock):
+        """A triple is returned if the area code is valid"""
+
+        def side_effect(*args, **_):
+            """Side effects local function"""
+
+            if args[0] == "urlfetch:get":
+                fixture = json.loads(
+                    helpers.getFixture("dbpedia-area-success.json")
+                )
+                return [fixture]
+            return mock.DEFAULT
+
+        publish_mock.side_effect = side_effect
+
+        result = self.plugin.state_by_area_code("212")
+
+        self.assertTrue("dbpedia" in result[0])
+        self.assertEqual(result[1], "NY")
+
+    @mock.patch("cherrypy.engine.publish")
+    def test_area_code_invalid(self, publish_mock):
+        """A triple is returned if the area code is valid"""
+
+        def side_effect(*args, **_):
+            """Side effects local function"""
+
+            if args[0] == "urlfetch:get":
+                fixture = json.loads(
+                    helpers.getFixture("dbpedia-area-fail.json")
+                )
+                return [fixture]
+            return mock.DEFAULT
+
+        publish_mock.side_effect = side_effect
+
+        result = self.plugin.state_by_area_code("000")
+
+        self.assertTrue("dbpedia" in result[0])
+        self.assertIsNone(result[1])
+        self.assertIsNone(result[2])
+
+    @mock.patch("cherrypy.engine.publish")
     def test_state_name_invalid(self, publish_mock):
         """An invalid state abbreviation returns Unknown for the state name"""
 
