6c46715688cefde8bb0b60914c2d3e88cfe98902
Bill Lovett <bill@ilovett.com>
2019-10-30 12:22:37 -0400

    Add types to hasher plugin

--- plugins/hasher.py
+++ plugins/hasher.py
@@ -1,16 +1,17 @@
 """Generate MD5 hashes."""
 
 import hashlib
+import typing
 import cherrypy
 
 
 class Plugin(cherrypy.process.plugins.SimplePlugin):
     """A CherryPy plugin for generating MD5 hashes."""
 
-    def __init__(self, bus):
+    def __init__(self, bus: cherrypy.process.wspbus.Bus) -> None:
         cherrypy.process.plugins.SimplePlugin.__init__(self, bus)
 
-    def start(self):
+    def start(self) -> None:
         """Define the CherryPy messages to listen for.
 
         This plugin owns the hasher prefix.
@@ -19,16 +20,26 @@ class Plugin(cherrypy.process.plugins.SimplePlugin):
         self.bus.subscribe("hasher:md5", self.md5_hash)
         self.bus.subscribe("hasher:sha256", self.sha256_hash)
 
-    def md5_hash(self, value, hex_digest=True):
+    def md5_hash(self, value: str, hex_digest: bool = True) -> str:
         """Calculate the MD5 digest of a value."""
-        return self._hash('md5', value, hex_digest)
+        return typing.cast(
+            str,
+            self._hash('md5', value, hex_digest)
+        )
 
-    def sha256_hash(self, value, hex_digest=True):
+    def sha256_hash(self, value: str, hex_digest: bool = True) -> str:
         """Calculate the SHA256 digest of a value."""
-        return self._hash('sha256', value, hex_digest)
+        return typing.cast(
+            str,
+            self._hash('sha256', value, hex_digest)
+        )
 
     @staticmethod
-    def _hash(algorithm, value, hex_digest=True):
+    def _hash(
+            algorithm: str,
+            value: str,
+            hex_digest: bool = True
+    ) -> typing.Union[str, bytes]:
         """Calculate the digest of a value using the specified algorithm."""
 
         hasher = hashlib.new(algorithm)
