387716c80f988dd8c2c5c0fb78f37a112e101588
Bill Lovett <bill@ilovett.com>
2019-11-12 11:31:47 -0500

    Add local_types.py omitted from previous commit

---
+++ local_types.py
@@ -0,0 +1,21 @@
+"""Mypy type types for global usage."""
+
+import typing
+
+
+NegotiableView = typing.Dict[str, str]
+
+Args = typing.List[typing.Any]
+
+Kwargs = typing.Dict[str, typing.Any]
+
+
+class Notification(typing.NamedTuple):
+    """Named tuple for a message payload to send to a notifier instance."""
+    group: str
+    title: str
+    body: str
+    badge: typing.Optional[str] = None
+    localId: typing.Optional[str] = None
+    expiresAt: typing.Optional[str] = None
+    url: typing.Optional[str] = None
