89fab1555c311259d34476305acc2720925f49ee
Bill Lovett <bill@ilovett.com>
2019-12-02 22:15:50 -0500

    Add types to capture tool

--- tools/capture.py
+++ tools/capture.py
@@ -1,10 +1,11 @@
+"""Save the headers and body of an incoming request for later review"""
 import cherrypy
 
 
 class Tool(cherrypy.Tool):
-    """Save the headers and body of an incoming request for later review"""
+    """A custom Cherrypy tool to store incoming requests via on_end_request."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         cherrypy.Tool.__init__(
             self,
             'on_end_request',
@@ -12,7 +13,8 @@ class Tool(cherrypy.Tool):
         )
 
     @staticmethod
-    def capture():
+    def capture() -> None:
+        """Send the Cherrypy request and response to the capture plugin."""
         cherrypy.engine.publish(
             "capture:add",
             cherrypy.request,
