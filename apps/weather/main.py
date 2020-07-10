4e336df3937926b3487d7345c7ca3e00812589be
Bill Lovett <bill@ilovett.com>
2020-07-09 21:56:03 -0400

    [weather] Replace DarkSky API with OpenWeather

--- apps/weather/main.py
+++ apps/weather/main.py
@@ -1,7 +1,5 @@
 """Current and upcoming forecasts"""
 
-import copy
-import math
 import typing
 from collections import defaultdict
 import cherrypy
@@ -17,7 +15,7 @@ class Controller:
 
     @cherrypy.tools.provides(formats=("html",))
     def GET(self, *args: str, **_kwargs: str) -> bytes:
-        """Display selected parts of the most recent Darksky API query"""
+        """Display selected parts of the most recent OpenWeather API query"""
 
         config = cherrypy.engine.publish(
             "registry:search:dict",
@@ -25,7 +23,7 @@ class Controller:
             key_slice=1
         ).pop()
 
-        if "darksky_key" not in config:
+        if "openweather_api_key" not in config:
             raise cherrypy.HTTPError(500, "No api key")
 
         locations = tuple(
@@ -55,7 +53,7 @@ class Controller:
                 and location[1] == longitude
             ), "")
 
-        cache_key = f"darksky_{latitude},{longitude}"
+        cache_key = f"openweather_{latitude},{longitude}"
 
         cached_api_response = cherrypy.engine.publish(
             "cache:get",
@@ -66,10 +64,11 @@ class Controller:
             forecast = self.shape_forecast(cached_api_response)
 
         if not cached_api_response:
-            endpoint = "https://api.darksky.net/forecast/"
-            endpoint += config['darksky_key']
-            endpoint += f"/{latitude},{longitude}"
-            endpoint += "?lang=en&units=us&exclude=minutely"
+            endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+            endpoint += f"?lat={latitude}&lon={longitude}"
+            endpoint += "&exclude=minutely"
+            endpoint += "&units=imperial"
+            endpoint += f"&appid={config['openweather_api_key']}"
 
             api_response = cherrypy.engine.publish(
                 "urlfetch:get",
@@ -113,110 +112,18 @@ class Controller:
 
         result: Forecast = defaultdict()
 
-        daily_block = forecast.get("daily", {})
-        days = daily_block.get("data", [{}, {}])
+        daily = forecast.get("daily", [])
+        hourly = forecast.get("hourly", [])
+        currently = forecast.get("current", {})
 
-        today = days[0]
-
-        currently = forecast.get("currently", {})
-        hourly = forecast.get("hourly", {})
-
-        result["current_summary"] = currently.get("summary")
-
-        result["upcoming"] = days[1:]
-
-        for item in result["upcoming"]:
-            item["time"] = cherrypy.engine.publish(
-                "clock:from_timestamp",
-                item.get("time"),
-                local=True
-            ).pop()
-
-            if "temperatureHigh" in item:
-                item["high"] = math.ceil(item.get("temperatureHigh"))
-
-            if "temperatureHighTime" in item:
-                item["high_at"] = cherrypy.engine.publish(
-                    "clock:from_timestamp",
-                    item.get("temperatureHighTime"),
-                    local=True
-                ).pop()
-
-            if "temperatureLow" in item:
-                item["low"] = math.ceil(item.get("temperatureLow"))
-
-            if "temperatureLowTime" in item:
-                item["low_at"] = cherrypy.engine.publish(
-                    "clock:from_timestamp",
-                    item.get("temperatureLowTime"),
-                    local=True
-                ).pop()
-
-        result["current_temperature"] = math.ceil(
-            currently.get("temperature")
-        )
-
-        result["current_time"] = cherrypy.engine.publish(
-            "clock:from_timestamp",
-            currently.get("time"),
+        now = cherrypy.engine.publish(
+            "clock:now",
             local=True
         ).pop()
 
-        result["current_humidity"] = currently.get("humidity", 0)
-
-        result["summary"] = today.get("summary")
-
-        result["temperature"] = math.ceil(today.get("temperature", 0))
-
-        result["sunrise"] = cherrypy.engine.publish(
-            "clock:from_timestamp",
-            today.get("sunriseTime"),
-            local=True
-        ).pop()
-
-        result["sunset"] = cherrypy.engine.publish(
-            "clock:from_timestamp",
-            today.get("sunsetTime"),
-            local=True
-        ).pop()
-
-        result["humidity"] = currently.get("humidity", 0)
-
-        result["high"] = math.ceil(today.get("temperatureHigh"))
-
-        result["high_at"] = cherrypy.engine.publish(
-            "clock:from_timestamp",
-            today.get("temperatureHighTime"),
-            local=True
-        ).pop()
-
-        result["low"] = math.ceil(today.get("temperatureLow"))
-
-        result["low_at"] = cherrypy.engine.publish(
-            "clock:from_timestamp",
-            today.get("temperatureLowTime"),
-            local=True
-        ).pop()
-
-        if "alerts" in forecast:
-            result["alerts"] = [
-                alert.get("description")
-                for alert in forecast["alerts"]
-            ]
-
-        now = cherrypy.engine.publish("clock:now", local=True).pop()
-        hours_remaining_today = 24 - now.hour
-
-        if "data" in hourly:
-            result["hourly"] = []
-            for hour in hourly["data"][0:hours_remaining_today]:
-                hour_clone = copy.copy(hour)
-                hour_clone["time"] = cherrypy.engine.publish(
-                    "clock:from_timestamp",
-                    hour_clone["time"],
-                    local=True
-                ).pop()
-
-                result["hourly"].append(hour_clone)
+        result["currently"] = currently
+        result["hourly"] = hourly[0:(24 - now.hour + 1)]
+        result["today"] = daily[0]
+        result["upcoming"] = daily[1:]
 
         return result
