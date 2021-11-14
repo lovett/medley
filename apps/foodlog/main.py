8b6543ae68f77260c1200969d9ded5db97988900
Bill Lovett <bill@ilovett.com>
2021-11-13 20:04:20 -0500

    [foodlog] Implement pydantic.

--- apps/foodlog/main.py
+++ apps/foodlog/main.py
@@ -1,8 +1,43 @@
 """Track food consumption."""
 
 import datetime
+from enum import Enum
 import re
 import cherrypy
+from pydantic import BaseModel
+from pydantic import ValidationError
+from pydantic import Field
+
+
+class Actions(str, Enum):
+    """Valid keywords for the first URL segment of this application."""
+    NONE = ""
+    NEW = "new"
+    EDIT = "edit"
+
+
+class DeleteParams(BaseModel):
+    """Valid request parameters for DELETE requests."""
+    uid: int = Field(0, gt=0)
+
+
+class GetParams(BaseModel):
+    """Valid request parameters for GET requests."""
+    uid: int = Field(0, gt=-1)
+    action: Actions = Actions.NONE
+    q: str = Field("", strip_whitespace=True, min_length=1, to_lower=True)
+    offset: int = 0
+    source: str = ""
+    per_page: int = 20
+
+
+class PostParams(BaseModel):
+    """Valid request parameters for POST requests."""
+    consume_date: datetime.date
+    consume_time: datetime.time
+    entry_id: int = Field(0, gt=-1)
+    foods_eaten: str = ""
+    overate: int = 0
 
 
 class Controller:
@@ -12,17 +47,17 @@ class Controller:
     show_on_homepage = True
 
     @staticmethod
-    def DELETE(*args: str) -> None:
+    def DELETE(uid: str = "0") -> None:
         """Remove an entry from the database."""
 
         try:
-            entry_id = int(args[0])
-        except (IndexError, ValueError) as error:
+            params = DeleteParams(uid=uid)
+        except ValidationError as error:
             raise cherrypy.HTTPError(400) from error
 
         result = cherrypy.engine.publish(
             "foodlog:remove",
-            entry_id
+            params.uid
         ).pop()
 
         if result:
@@ -32,42 +67,45 @@ class Controller:
         raise cherrypy.HTTPError(404)
 
     @cherrypy.tools.provides(formats=("html",))
-    def GET(self, *args: str, **kwargs: str) -> bytes:
+    def GET(self, uid: str = "0", action: str = "", **kwargs: str) -> bytes:
         """Dispatch to a subhandler based on the URL path."""
 
         try:
-            if args[0] == "new":
-                return self.form(0, **kwargs)
-
-            if args[-1] == "edit":
-                entry_id = int(args[-2])
-                return self.form(entry_id, **kwargs)
-
-            if args[0] == "search":
-                query = kwargs.get("q", "")
-                return self.search(query, **kwargs)
-
-            raise cherrypy.HTTPError(404)
-
-        except ValueError as error:
+            params = GetParams(
+                uid=uid,
+                action=action,
+                **kwargs
+            )
+        except ValidationError as error:
             raise cherrypy.HTTPError(400) from error
 
-        except IndexError:
-            return self.index(**kwargs)
+        if params.action == Actions.NEW:
+            params.uid = 0
+            return self.form(params)
+
+        if params.action == Actions.EDIT:
+            return self.form(params)
+
+        if params.q:
+            return self.search(params)
+
+        return self.index(params)
 
     @staticmethod
-    def POST(**kwargs: str) -> None:
+    def POST(uid: str = "0", **kwargs: str) -> None:
         """Add a new entry or update an existing one."""
 
-        consume_date = kwargs.get("consume_date")
-        consume_time = kwargs.get("consume_time")
-        entry_id = int(kwargs.get("entry_id", 0))
-        foods_eaten = kwargs.get("foods_eaten")
-        overate = int(kwargs.get("overate", 0))
+        try:
+            params = PostParams(
+                uid=uid,
+                **kwargs
+            )
+        except ValidationError as error:
+            raise cherrypy.HTTPError(400) from error
 
-        consumed_on = datetime.datetime.strptime(
-            f"{consume_date} {consume_time}",
-            "%Y-%m-%d %H:%M",
+        consumed_on = datetime.datetime.combine(
+            params.consume_date,
+            params.consume_time
         )
 
         consumed_on_utc = cherrypy.engine.publish(
@@ -77,10 +115,10 @@ class Controller:
 
         result = cherrypy.engine.publish(
             "foodlog:upsert",
-            entry_id,
+            params.entry_id,
             consumed_on=consumed_on_utc,
-            foods_eaten=foods_eaten,
-            overate=overate
+            foods_eaten=params.foods_eaten,
+            overate=params.overate
         ).pop()
 
         if not result:
@@ -94,15 +132,12 @@ class Controller:
         raise cherrypy.HTTPRedirect(redirect_url)
 
     @staticmethod
-    def index(*_args: str, **kwargs: str) -> bytes:
+    def index(params: GetParams) -> bytes:
         """The application's default view."""
-
-        offset = int(kwargs.get("offset", 0))
-        limit = 20
 
         (entries, entry_count) = cherrypy.engine.publish(
             "foodlog:search:date",
-            offset=offset
+            offset=params.offset
         ).pop()
 
         pagination_url = cherrypy.engine.publish(
@@ -115,13 +150,13 @@ class Controller:
             entries=entries,
             entry_count=entry_count,
             pagination_url=pagination_url,
-            offset=offset,
-            per_page=limit,
+            offset=params.offset,
+            per_page=params.per_page,
             allow_add=True
         ).pop()
 
     @staticmethod
-    def form(entry_id: int = 0, **_kwargs: str) -> bytes:
+    def form(params: GetParams) -> bytes:
         """Display a form for adding or updating an entry."""
 
         entry_date = cherrypy.engine.publish(
@@ -129,15 +164,12 @@ class Controller:
             local=True
         ).pop()
 
-        foods_eaten = ""
-        overate = 0
-
         allow_delete = False
-        if entry_id:
+        if params.uid:
             allow_delete = True
             entry = cherrypy.engine.publish(
                 "foodlog:find",
-                entry_id
+                params.uid
             ).pop()
 
             if not entry:
@@ -153,53 +185,41 @@ class Controller:
             allow_cancel=True,
             allow_add=False,
             allow_delete=allow_delete,
-            entry_id=entry_id,
+            entry_id=params.uid,
             entry_date=entry_date,
             foods_eaten=foods_eaten,
             overate=overate
         ).pop()
 
     @staticmethod
-    def search(query: str = "", **kwargs: str) -> bytes:
+    def search(params: GetParams) -> bytes:
         """Display entries matching a search."""
 
-        offset = int(kwargs.get("offset", 0))
-        limit = 20
-
-        if not query:
-            redirect_url = cherrypy.engine.publish(
-                "app_url"
-            ).pop()
-
-            raise cherrypy.HTTPRedirect(redirect_url)
-
-        query = query.lower().strip()
-
-        if re.fullmatch(r"\d{4}-\w{2}-\d{2}", query):
+        if re.fullmatch(r"\d{4}-\w{2}-\d{2}", params.q):
             search_term = cherrypy.engine.publish(
                 "clock:from_format",
-                query,
+                params.q,
                 "%Y-%m-%d"
             ).pop()
 
             (entries, entry_count) = cherrypy.engine.publish(
                 "foodlog:search:date",
                 query=search_term,
-                offset=offset,
-                limit=limit
+                offset=params.offset,
+                limit=params.per_page
             ).pop()
         else:
             (entries, entry_count) = cherrypy.engine.publish(
                 "foodlog:search:keyword",
-                query=query,
-                offset=offset,
-                limit=limit
+                query=params.q,
+                offset=params.offset,
+                limit=params.per_page
             ).pop()
 
         pagination_url = cherrypy.engine.publish(
             "app_url",
             "/foodlog/search",
-            {"q": query}
+            {"q": params.q}
         ).pop()
 
         return cherrypy.engine.publish(
@@ -207,9 +227,9 @@ class Controller:
             "apps/foodlog/foodlog-index.jinja.html",
             entries=entries,
             entry_count=entry_count,
-            query=query,
-            offset=offset,
-            per_page=limit,
+            query=params.q,
+            offset=params.offset,
+            per_page=params.per_page,
             pagination_url=pagination_url,
             allow_cancel=True,
             allow_add=True
