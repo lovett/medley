f7482c2d15b81b9ef05ad2a68b64c39c089b9d65
Bill Lovett <bill@ilovett.com>
2023-09-24 11:37:48 -0400

    [startpage] Get rid of anonymizer deactivation checkbox.
    I never use it.

--- apps/static/js/startpage.js
+++ apps/static/js/startpage.js
@@ -1,9 +1,6 @@
 MEDLEY.startpage = (function () {
     'use strict';
 
-    let anonymizerUrl = null;
-    let pageLinks = [];
-
     /**
      * Open multiple links at once.
      */
@@ -26,54 +23,8 @@ MEDLEY.startpage = (function () {
         window.location.href = links[0].getAttribute('href');
     }
 
-    /**
-     * Prefix or unprefix the anonymizer URL from eligible links.
-     */
-    function toggleAnonymizer (e) {
-        for (let i=0; i < pageLinks.length; i++) {
-            const link = pageLinks[i];
-            if (parseInt(link.dataset.anonable, 10) === -1) {
-                continue;
-            }
-
-            let href = decodeURIComponent(link.getAttribute('href'));
-            href = href.replace(anonymizerUrl, '');
-
-            if (e.target.checked) {
-                href = anonymizerUrl + encodeURIComponent(href);
-            }
-
-            link.setAttribute('href', href);
-        }
-    }
-
     return {
         init: function () {
-            pageLinks = document.getElementsByTagName('A');
-
-            // Determine the anonymizer URL
-            const nodes = document.getElementsByTagName('META');
-            for (let i=0; i < nodes.length; i++) {
-                if (nodes[i].getAttribute('name') === 'anonymizer') {
-                    anonymizerUrl = nodes[i].getAttribute('content');
-                    break;
-                }
-            }
-
-            if (anonymizerUrl) {
-                const anonymizeCheckbox = document.getElementById('anonymize');
-                anonymizeCheckbox.removeAttribute('disabled');
-                anonymizeCheckbox.setAttribute('checked', true);
-
-                // Links that are not initially anonymized should always remain so
-                for (let i=0; i < pageLinks.length; i++) {
-                    const href = pageLinks[i].getAttribute('href');
-                    pageLinks[i].dataset.anonable = href.indexOf(anonymizerUrl);
-                }
-
-                anonymizeCheckbox.addEventListener('click', toggleAnonymizer);
-            }
-
             // Tag link groups
             const lis = document.getElementsByTagName('LI');
             const groupTrigger = document.createElement('A');
